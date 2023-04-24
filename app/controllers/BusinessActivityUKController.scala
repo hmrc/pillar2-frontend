80cbd7a9adcce1b04093f95f8817494eb8be520b
Javed Iqbal <javed.iqbal1@digital.hmrc.gov.uk>
2023-04-24 13:19:57 +0100

    PILL-117 Does MNE have business activity in UK

---
+++ app/controllers/BusinessActivityUKController.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.FrontendAppConfig
+import connectors.UserAnswersConnectors
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import forms.BusinessActivityUKFormProvider
+
+import javax.inject.Inject
+import models.Mode
+import navigation.Navigator
+import pages.BusinessActivityUKPage
+import play.api.i18n.I18nSupport
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.BusinessActivityUKView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class BusinessActivityUKController @Inject() (
+  val userAnswersConnectors: UserAnswersConnectors,
+  navigator:                 Navigator,
+  identify:                  IdentifierAction,
+  getData:                   DataRetrievalAction,
+  requireData:               DataRequiredAction,
+  formProvider:              BusinessActivityUKFormProvider,
+  val controllerComponents:  MessagesControllerComponents,
+  view:                      BusinessActivityUKView
+)(implicit ec:               ExecutionContext, appConfig: FrontendAppConfig)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  val form = formProvider()
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    val preparedForm = request.userAnswers.get(BusinessActivityUKPage) match {
+      case None        => form
+      case Some(value) => form.fill(value)
+    }
+
+    Ok(view(preparedForm, mode))
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode))),
+        value =>
+          for {
+
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(BusinessActivityUKPage, value))
+            _              <- userAnswersConnectors.save(updatedAnswers.id, Json.toJson(updatedAnswers.data))
+          } yield Redirect(routes.CheckYourAnswersController.onPageLoad)
+      )
+  }
+}
