7129917ad7d47243695b94f33fd0924f59bb9df6
alexbrook3 <127312214+alexbrook3@users.noreply.github.com>
2023-04-28 16:12:14 +0100

    PIL-118: [AB] updated Usr Ans Gen

--- test-utils/generators/UserAnswersGenerator.scala
+++ test-utils/generators/UserAnswersGenerator.scala
@@ -27,7 +27,6 @@ trait UserAnswersGenerator extends TryValues {
   self: Generators =>
 
   val generators: Seq[Gen[(QuestionPage[_], JsValue)]] =
-    arbitrary[(GroupTerritoriesPage.type, JsValue)] ::
       Nil
 
   implicit lazy val arbitraryUserData: Arbitrary[UserAnswers] = {
