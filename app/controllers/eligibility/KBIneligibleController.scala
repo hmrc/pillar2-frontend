9d49a2d1ae699a7f33dcca2a66cd65f4b39544e3
Javed Iqbal <javed.iqbal1@digital.hmrc.gov.uk>
2023-05-05 13:01:20 +0100

    [PIL-235][JA] fix error

--- app/controllers/eligibility/KBIneligibleController.scala
+++ app/controllers/eligibility/KBIneligibleController.scala
@@ -20,13 +20,13 @@ import config.FrontendAppConfig
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.{KBIneligible, UnderConstruction}
+import views.html.KBIneligibleView
 
 import javax.inject.Inject
 
 class KBIneligibleController @Inject() (
   val controllerComponents: MessagesControllerComponents,
-  view:                     KBIneligible
+  view:                     KBIneligibleView
 )(implicit appConfig:       FrontendAppConfig)
     extends FrontendBaseController
     with I18nSupport {
