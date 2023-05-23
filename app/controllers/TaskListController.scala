dc729f9cdc55dadd48644f99456c6d8de0ccc0d6
Javed Iqbal <javed.iqbal1@digital.hmrc.gov.uk>
2023-05-23 14:17:23 +0100

    [PIL-2057][JA] initial key

--- app/controllers/TaskListController.scala
+++ app/controllers/TaskListController.scala
@@ -14,24 +14,31 @@
  * limitations under the License.
  */
 
-package controllers.auth
+package controllers
 
 import config.FrontendAppConfig
-
-import javax.inject.Inject
+import controllers.actions.IdentifierAction
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.auth.SignedOutView
+import views.html.IndexView
 
-class SignedOutController @Inject() (
+import javax.inject.Inject
+
+class TaskListController @Inject() (
   val controllerComponents: MessagesControllerComponents,
-  view:                     SignedOutView
+  identify:                 IdentifierAction,
+  view:                     IndexView
 )(implicit appConfig:       FrontendAppConfig)
     extends FrontendBaseController
     with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = Action { implicit request =>
+  def onPageLoad: Action[AnyContent] = identify { implicit request =>
     Ok(view())
   }
+
+  def onSubmit: Action[AnyContent] = identify { implicit request =>
+    Redirect(routes.TradingBusinessConfirmationController.onPageLoad)
+  }
+
 }
