b95476376d94f85511c0d228629f8f80103f5179
Al-kouhestani <90757950+Al-kouhestani@users.noreply.github.com>
2023-06-22 10:40:59 +0100

    stringPage controller

--- .g8/stringPage/app/controllers/$className$Controller.scala
+++ .g8/stringPage/app/controllers/$className$Controller.scala
@@ -1,29 +1,30 @@
 package controllers
 
+import controllers.actions._
 import forms.$className$FormProvider
+import connectors.UserAnswersConnectors
+import config.FrontendAppConfig
 import javax.inject.Inject
 import models.Mode
-import navigation.Navigator
 import pages.$className$Page
-import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import repositories.SessionRepository
+import play.api.libs.json.Format.GenericFormat
+import play.api.libs.json.Json
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.$className$View
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class $className$Controller @Inject()(
-                                        override val messagesApi: MessagesApi,
-                                        sessionRepository: SessionRepository,
-                                        navigator: Navigator,
-                                        identify: IdentifierAction,
-                                        getData: DataRetrievalAction,
-                                        requireData: DataRequiredAction,
-                                        formProvider: $className$FormProvider,
-                                        val controllerComponents: MessagesControllerComponents,
-                                        view: $className$View
-                                    )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+class $className;format="cap"$$Controller @Inject()(
+                                       val userAnswersConnectors: UserAnswersConnectors,
+                                       identify: IdentifierAction,
+                                       getData: DataRetrievalAction,
+                                       requireData: DataRequiredAction,
+                                       formProvider: $className$FormProvider,
+                                       val controllerComponents: MessagesControllerComponents,
+                                       view: $className$View
+                                     )(implicit ec: ExecutionContext, appConfig: FrontendAppConfig) extends FrontendBaseController with I18nSupport {
 
   val form = formProvider()
 
@@ -48,8 +49,8 @@ class $className$Controller @Inject()(
         value =>
           for {
             updatedAnswers <- Future.fromTry(request.userAnswers.set($className$Page, value))
-            _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage($className$Page, mode, updatedAnswers))
+                        _  <- userAnswersConnectors.save (updatedAnswers.id, Json.toJson (updatedAnswers.data) )
+              } yield Redirect (routes.UnderConstructionController.onPageLoad)
       )
   }
 }
