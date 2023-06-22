2df6d0375e15d62f3f1fbdb49a2414dfc03b9d27
Al-kouhestani <90757950+Al-kouhestani@users.noreply.github.com>
2023-06-22 14:19:30 +0100

    contentPage complted

--- .g8/contentPage/app/controllers/$className$Controller.scala
+++ .g8/contentPage/app/controllers/$className$Controller.scala
@@ -1,19 +1,18 @@
 package controllers
 
 import javax.inject.Inject
-import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.$className$View
+import config.FrontendAppConfig
 
 class $className$Controller @Inject()(
-                                       override val messagesApi: MessagesApi,
-                                       identify: IdentifierAction,
                                        getData: DataRetrievalAction,
                                        requireData: DataRequiredAction,
                                        val controllerComponents: MessagesControllerComponents,
                                        view: $className$View
-                                     ) extends FrontendBaseController with I18nSupport {
+                                     )(appConfig: FrontendAppConfig)) extends FrontendBaseController with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
