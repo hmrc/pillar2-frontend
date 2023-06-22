bc90c86d259a59b5f4223e29f69d02561ec3eb82
Al-kouhestani <90757950+Al-kouhestani@users.noreply.github.com>
2023-06-22 15:54:41 +0100

    modified viewInstances

--- test/helpers/ViewInstances.scala
+++ test/helpers/ViewInstances.scala
@@ -62,7 +62,7 @@ trait ViewInstances extends Configs with StubMessageControllerComponents {
   val govukHint         = new GovukHint
   val govukRadios       = new GovukRadios(new GovukErrorMessage, new GovukFieldset, new GovukHint, new GovukLabel)
   val govukInput        = new GovukInput(new GovukErrorMessage, new GovukHint, new GovukLabel)
-  val govukDateInput    = new GovukDateInput(new GovukErrorMessage,  new GovukHint, new GovukFieldset, govukInput)
+  val govukDateInput    = new GovukDateInput(new GovukErrorMessage, new GovukHint, new GovukFieldset, govukInput)
   val govukCheckboxes   = new GovukCheckboxes(new GovukErrorMessage, new GovukFieldset, new GovukHint, new GovukLabel)
   val govukLabel        = new GovukLabel()
   val govukDetails      = new GovukDetails
