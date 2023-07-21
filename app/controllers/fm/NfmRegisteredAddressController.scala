3f29c16d72b885efbfbb27ce165adcdae4cc5212
Javed Iqbal <javed.iqbal1@digital.hmrc.gov.uk>
2023-07-21 12:17:01 +0100

    [PIL330][JA] filing member address page

---
+++ app/controllers/fm/NfmRegisteredAddressController.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.fm
+
+import config.FrontendAppConfig
+import connectors.UserAnswersConnectors
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import forms.UpeRegisteredAddressFormProvider
+import models.requests.DataRequest
+import models.{Mode, UpeRegisteredAddress}
+import navigation.Navigator
+import pages.RegistrationPage
+import play.api.data.Form
+import play.api.i18n.I18nSupport
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.registrationview.UpeRegisteredAddressView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class NfmRegisteredAddressController @Inject()(
+  val userAnswersConnectors: UserAnswersConnectors,
+  navigator:                 Navigator,
+  identify:                  IdentifierAction,
+  getData:                   DataRetrievalAction,
+  requireData:               DataRequiredAction,
+  formProvider:              UpeRegisteredAddressFormProvider,
+  val controllerComponents:  MessagesControllerComponents,
+  view:                      UpeRegisteredAddressView
+)(implicit ec:               ExecutionContext, appConfig: FrontendAppConfig)
+    extends FrontendBaseController
+    with I18nSupport {
+  val form: Form[UpeRegisteredAddress] = formProvider()
+  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    val userName = getUserName(request)
+
+    val preparedForm = request.userAnswers.get(RegistrationPage) match {
+      case None        => form
+      case Some(value) => value.withoutIdRegData.fold(form)(data => data.upeRegisteredAddress.fold(form)(address => form.fill(address)))
+    }
+
+    Ok(view(preparedForm, mode, userName))
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    val userName = getUserName(request)
+    form
+      .bindFromRequest()
+      .fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode, userName))),
+        value => {
+          val regData          = request.userAnswers.get(RegistrationPage).getOrElse(throw new Exception("Is UPE registered in UK not been selected"))
+          val regDataWithoutId = regData.withoutIdRegData.getOrElse(throw new Exception("upeNameRegistration should be available before address"))
+          for {
+            updatedAnswers <-
+              Future.fromTry(
+                request.userAnswers
+                  .set(RegistrationPage, regData.copy(withoutIdRegData = Some(regDataWithoutId.copy(upeRegisteredAddress = Some(value)))))
+              )
+            _ <- userAnswersConnectors.save(updatedAnswers.id, Json.toJson(updatedAnswers.data))
+          } yield Redirect(controllers.registration.routes.UpeContactNameController.onPageLoad(mode))
+        }
+      )
+  }
+
+  private def getUserName(request: DataRequest[AnyContent]): String = {
+    val registration = request.userAnswers.get(RegistrationPage)
+    registration.fold("")(regData => regData.withoutIdRegData.fold("")(withoutId => withoutId.upeNameRegistration))
+  }
+}
---
+++ app/forms/NfmRegisteredAddressFormProvider.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.mappings.Mappings
+import models.UpeRegisteredAddress
+import play.api.data.Form
+import play.api.data.Forms.{mapping, optional}
+
+import javax.inject.Inject
+
+class NfmRegisteredAddressFormProvider @Inject() extends Mappings {
+  private val textLength = 200
+  def apply(): Form[UpeRegisteredAddress] = Form(
+    mapping(
+      "addressLine1" ->
+        text("upe-registered-address.messages.error.address-line-1.required")
+          .verifying(maxLength(textLength, "upe-registered-address.messages.error.address-line-1.length")),
+      "addressLine2" -> optional(
+        text("")
+          .verifying(maxLength(textLength, "upe-registered-address.messages.error.address-line-2.length"))
+      ),
+      "addressLine3" ->
+        text("upe-registered-address.town-city.error.required")
+          .verifying(maxLength(textLength, "upe-registered-address.town-city.error.length")),
+      "addressLine4" ->
+        optional(
+          text("")
+            .verifying(maxLength(textLength, "upe-registered-address.region.error.length"))
+        ),
+      "postalCode" ->
+        optional(
+          text("")
+            .verifying(maxLength(textLength, "upe-registered-address.postcode.error.length"))
+        ),
+      "countryCode" ->
+        text("upe-registered-address.country.error.required")
+          .verifying(maxLength(textLength, "upe-registered-address.country.error.length"))
+    )(UpeRegisteredAddress.apply)(UpeRegisteredAddress.unapply)
+  )
+}
