83aa30b0c9ae1009c030f17f653e2f4e9a156a1d
nitin-srivastav <127774818+nitin-srivastav@users.noreply.github.com>
2023-09-11 11:01:55 +0100

    PIL-168:[NS] add unit test

--- test/controllers/GroupAccountingPeriodControllerSpec.scala
+++ test/controllers/GroupAccountingPeriodControllerSpec.scala
@@ -17,11 +17,16 @@
 package controllers
 
 import base.SpecBase
+import connectors.UserAnswersConnectors
 import forms.GroupAccountingPeriodFormProvider
 import models.fm.FilingMember
-import models.subscription.Subscription
+import models.subscription.{AccountingPeriod, Subscription}
 import models.{MneOrDomestic, NfmRegistrationConfirmation, NormalMode, UserAnswers}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
 import pages.{NominatedFilingMemberPage, SubscriptionPage}
+import play.api.inject.bind
+import play.api.libs.json.Json
 import play.api.mvc.{AnyContentAsEmpty, AnyContentAsFormUrlEncoded}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
@@ -29,7 +34,7 @@ import utils.RowStatus
 import views.html.subscriptionview.GroupAccountingPeriodView
 
 import java.time.{LocalDate, ZoneOffset}
-
+import scala.concurrent.Future
 class GroupAccountingPeriodControllerSpec extends SpecBase {
 
   val formProvider = new GroupAccountingPeriodFormProvider()
@@ -46,9 +51,9 @@ class GroupAccountingPeriodControllerSpec extends SpecBase {
   def postRequest(): FakeRequest[AnyContentAsFormUrlEncoded] =
     FakeRequest(POST, controllers.subscription.routes.GroupAccountingPeriodController.onPageLoad(NormalMode).url)
       .withFormUrlEncodedBody(
-        "value.day"   -> validAnswer.getDayOfMonth.toString,
-        "value.month" -> validAnswer.getMonthValue.toString,
-        "value.year"  -> validAnswer.getYear.toString
+        "startDay.day"   -> validAnswer.getDayOfMonth.toString,
+        "startDay.month" -> validAnswer.getMonthValue.toString,
+        "startDay.year"  -> validAnswer.getYear.toString
       )
 
   "GroupAccountingPeriod Controller" when {
@@ -74,6 +79,47 @@ class GroupAccountingPeriodControllerSpec extends SpecBase {
         status(result) mustEqual OK
         contentAsString(result) mustEqual view(formProvider(), NormalMode)(getRequest, appConfig(application), messages(application)).toString
       }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val userAnswersWithNominatedFilingMemberWithSub =
+        userAnswersNfmNoId.set(SubscriptionPage, validSubscriptionDataWithAccountingPeriod).success.value
+
+      val userAnswers =
+        UserAnswers(userAnswersId)
+          .set(
+            SubscriptionPage,
+            Subscription(
+              domesticOrMne = MneOrDomestic.Uk,
+              RowStatus.InProgress,
+              accountingPeriod = Some(AccountingPeriod(LocalDate.now(ZoneOffset.UTC), LocalDate.now(ZoneOffset.UTC)))
+            )
+          )
+          .success
+          .value
+          .set(NominatedFilingMemberPage, FilingMember(nfmConfirmation = NfmRegistrationConfirmation.Yes, isNFMnStatus = RowStatus.Completed))
+          .success
+          .value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers))
+        .overrides(bind[UserAnswersConnectors].toInstance(mockUserAnswersConnectors))
+        .build()
+
+      running(application) {
+        when(mockUserAnswersConnectors.save(any(), any())(any())).thenReturn(Future(Json.toJson(Json.obj())))
+
+        val request = FakeRequest(POST, controllers.subscription.routes.GroupAccountingPeriodController.onSubmit(NormalMode).url)
+          .withFormUrlEncodedBody(
+            "startDate" -> validAnswer.toString
+          )
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.UnderConstructionController.onPageLoad.url
+      }
+
     }
 
     "must return a Bad Request and errors when invalid data is submitted" in {
