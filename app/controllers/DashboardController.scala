09b0af7019bd2198503b4354afe4cbd913daa76c
olakunlekuye2 <olakunle.kuye2@digital.hmrc.gov.uk>
2023-11-15 14:10:37 +0000

    Updated DashboardController

--- app/controllers/DashboardController.scala
+++ app/controllers/DashboardController.scala
@@ -23,6 +23,7 @@ import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.DashboardView
 import config.FrontendAppConfig
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import models.UserAnswers
 import models.registration.RegistrationInfo
 import models.subscription.ReadSubscriptionRequestParameters
 import pages.{UpeRegInformationPage, upeNameRegistrationPage}
@@ -45,32 +46,75 @@ class DashboardController @Inject() (
     extends FrontendBaseController
     with I18nSupport
     with Logging {
+
   def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
-    val userId         = request.userId
-    val identifierName = "PLRID"
+    val userId = request.userId
     val plrReference = request.enrolments
-      .flatMap(_.find(_.key.equalsIgnoreCase(identifierName)))
-      .flatMap(_.identifiers.find(_.key.equalsIgnoreCase(identifierName)))
+      .flatMap(_.find { enrolment =>
+        enrolment.key.equalsIgnoreCase("HMRC-PLR-ORG") &&
+        enrolment.identifiers.exists(_.key.equalsIgnoreCase("PLRID"))
+      })
+      .flatMap(_.identifiers.find(_.key.equalsIgnoreCase("PLRID")))
       .map(_.value)
-      .getOrElse(identifierName)
 
-    val readSubscriptionParameters = ReadSubscriptionRequestParameters(userId, plrReference)
+    plrReference match {
+      case Some(ref) =>
+        for {
+          readSub <- readSubscriptionService.readSubscription(ReadSubscriptionRequestParameters(userId, ref))
+          result <- readSub match {
+                      case Right(userAnswers) =>
+                        getSome(userAnswers) match {
+                          case Some((companyName, regDate)) =>
+                            Future.successful(Ok(view(companyName, regDate.format(DateTimeFormatter.ofPattern("d MMMM yyyy")), ref)))
+                          case None => Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
+                        }
+                      case Left(error) =>
+                        logger.error(s"Error retrieving subscription: $error")
+                        Future.successful(InternalServerError("Internal Server Error occurred"))
+                    }
+        } yield result
 
-    readSubscriptionService.readSubscription(readSubscriptionParameters).flatMap {
-      case Right(userAnswers) =>
-        val organisationName = userAnswers.get[String](upeNameRegistrationPage).getOrElse("Default Organisation Name")
-
-        val registrationDateFormatted = userAnswers
-          .get[RegistrationInfo](UpeRegInformationPage)
-          .flatMap(_.registrationDate)
-          .map(_.format(DateTimeFormatter.ofPattern("d MMMM yyyy")))
-          .getOrElse("Default Date")
-
-        Future.successful(Ok(view(organisationName, registrationDateFormatted, plrReference)))
-
-      case Left(error) =>
-        logger.error(s"Error retrieving subscription  $error")
-        Future.successful(InternalServerError("Subscription not found in user answers"))
+      case None =>
+        Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
     }
   }
+
+  private def getSome(userAnswers: UserAnswers) =
+    (userAnswers.get(upeNameRegistrationPage), userAnswers.get(UpeRegInformationPage).flatMap(_.registrationDate)) match {
+      case (Some(x), Some(y)) => Option(x, y)
+      case _                  => None
+    }
+
+//  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+//    val plrReference = request.enrolments
+//      .flatMap(_.find { enrolment =>
+//        enrolment.key.equalsIgnoreCase("HMRC-PLR-ORG") &&
+//          enrolment.identifiers.exists(_.key.equalsIgnoreCase("PLRID"))
+//      })
+//      .flatMap(_.identifiers.find(_.key.equalsIgnoreCase("PLRID")))
+//      .map(_.value)
+//    for {
+//      ref <- plrReference
+//      userId = request.userId
+//      readSub <- readSubscriptionService.readSubscription(ReadSubscriptionRequestParameters(userId, ref))
+//    } yield {
+//      readSub match {
+//        case Right(userAnswers) => getSome(userAnswers) match {
+//          case Some(Tuple2(companyName, regDate)) => Ok(view(companyName, regDate.format(DateTimeFormatter.ofPattern("d MMMM yyyy")), ref))
+//          case None => Redirect(controllers.routes.JourneyRecoveryController.onPageLoad())
+//        }
+//        case Left(error) => logger.error(s"Error retrieving subscription  $error")
+//          Future.successful(InternalServerError())
+//      }
+//
+//    }
+//  }
+
+//  private def getSome(userAnswers: UserAnswers) = {
+//    (userAnswers.get(upeNameRegistrationPage), userAnswers.get(UpeRegInformationPage).flatMap(_.registrationDate)) match {
+//      case (Some(x), Some(y)) => Option(x, y)
+//      case _ => None
+//    }
+//  }
+
 }
