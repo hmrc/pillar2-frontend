9a8475582214a6e8eb3fca4fca8a010ce221e6aa
olakunlekuye2 <olakunle.kuye2@digital.hmrc.gov.uk>
2023-12-21 22:52:30 +0000

    [PIL-471][OK] Updated View

--- app/controllers/TaskListController.scala
+++ app/controllers/TaskListController.scala
@@ -18,13 +18,12 @@ package controllers
 
 import config.FrontendAppConfig
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
-import helpers.SubscriptionHelpers
-import play.api.i18n.I18nSupport
-import play.api.mvc.Results.Redirect
+import models.requests.DataRequest
+import play.api.Logging
+import play.api.i18n.{I18nSupport, Messages}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import utils.{Pillar2SessionKeys, RowStatus}
-import utils.RowStatus._
 import views.html.TaskListView
 
 import javax.inject.Inject
@@ -37,46 +36,59 @@ class TaskListController @Inject() (
   view:                     TaskListView
 )(implicit appConfig:       FrontendAppConfig)
     extends FrontendBaseController
-    with I18nSupport {
+    with I18nSupport
+    with Logging {
+
+  private def getStatusMessage(status: RowStatus)(implicit messages: Messages): String = status match {
+    case RowStatus.Completed  => messages("task.status.completed")
+    case RowStatus.InProgress => messages("task.status.inProgress")
+    case RowStatus.NotStarted => messages("task.status.notStarted")
+    case _                    => messages("task.status.cannotStartYet") // For other cases, such as "Cannot start yet"
+  }
+
+  private def getCyaStatusMessage(status: String)(implicit messages: Messages): String =
+    status match {
+      case "Completed"  => Messages("task.status.completed")
+      case "InProgress" => Messages("task.status.inProgress")
+      case _            => Messages("task.status.notStarted") // Adjust for "Cannot start yet"
+    }
 
   def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
-    val upeStatus             = request.userAnswers.upeStatus
-    val fmStatus              = request.userAnswers.fmStatus
-    val groupDetailStatus     = request.userAnswers.groupDetailStatus
-    val contactDetailsStatus  = request.userAnswers.contactDetailStatus
-    val reviewAndSubmitStatus = request.userAnswers.finalCYAStatus(upeStatus, fmStatus, groupDetailStatus, contactDetailsStatus)
-    val count                 = statusCounter(upeStatus, fmStatus, groupDetailStatus, contactDetailsStatus, NotStarted)
-    if (request.session.get(Pillar2SessionKeys.plrId).isDefined) {
-      Redirect(routes.RegistrationConfirmationController.onPageLoad)
-    } else {
-      Ok(
-        view(
-          upeStatus.toString,
-          count,
-          filingMemberStatus = fmStatus.toString,
-          groupDetailStatus = groupDetailStatus.toString,
-          contactDetailsStatus = contactDetailsStatus.toString,
-          reviewAndSubmitStatus
-        )
+    val upeStatus            = request.userAnswers.upeStatus
+    val fmStatus             = request.userAnswers.fmStatus
+    val groupDetailStatus    = request.userAnswers.groupDetailStatus
+    val contactDetailsStatus = request.userAnswers.contactDetailStatus
+
+    // Calculate the CYA status using the previously calculated string
+    val reviewAndSubmitStatusString = request.userAnswers.finalCYAStatus(upeStatus, fmStatus, groupDetailStatus, contactDetailsStatus)
+
+    // Get status messages for each section
+    val ultimateParentStatusMessage = getStatusMessage(upeStatus)
+    val filingMemberStatusMessage   = getStatusMessage(fmStatus)
+    val groupDetailsStatusMessage   = getStatusMessage(groupDetailStatus)
+    val contactDetailsStatusMessage = getStatusMessage(contactDetailsStatus)
+    val cyaSectionStatusMessage     = getCyaStatusMessage(reviewAndSubmitStatusString) // Use the calculated status string here
+
+    logger.info(s"ultimateParentStatusMessage: $ultimateParentStatusMessage")
+    logger.info(s"filingMemberStatusMessage: $filingMemberStatusMessage")
+    logger.info(s"groupDetailsStatusMessage: $groupDetailsStatusMessage")
+    logger.info(s"contactDetailsStatusMessage: $contactDetailsStatusMessage")
+    logger.info(s"cyaSectionStatusMessage: $cyaSectionStatusMessage")
+
+    // Calculate the number of completed sections
+    val count = List(upeStatus, fmStatus, groupDetailStatus, contactDetailsStatus)
+      .count(_ == RowStatus.Completed)
+
+    // Render the view with the required parameters
+    Ok(
+      view(
+        ultimateParentStatusMessage,
+        count,
+        filingMemberStatusMessage,
+        groupDetailsStatusMessage,
+        contactDetailsStatusMessage,
+        cyaSectionStatusMessage
       )
-    }
+    )
   }
-
-  private def statusCounter(
-    registrationStatus: RowStatus,
-    filingMemberStatus: RowStatus,
-    suscriptionStatus:  RowStatus,
-    contactStatus:      RowStatus,
-    cyaStatus:          RowStatus
-  ): Int = {
-    val statusList = List(registrationStatus, filingMemberStatus, suscriptionStatus, contactStatus, cyaStatus)
-    var counter    = 0
-    for (task <- statusList)
-      if (task == Completed)
-        counter += 1
-      else
-        counter
-    counter
-  }
-
 }
