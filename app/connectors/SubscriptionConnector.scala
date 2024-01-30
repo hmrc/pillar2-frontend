6226fae0fa5fb7750e843540516aff1b1db16b0a
olakunlekuye2 <olakunle.kuye2@digital.hmrc.gov.uk>
2024-01-30 11:29:57 +0000

    [PIL-634][OK] - updated http read

--- app/connectors/SubscriptionConnector.scala
+++ app/connectors/SubscriptionConnector.scala
@@ -20,50 +20,54 @@ import config.FrontendAppConfig
 import models.subscription.{ErrorResponse, SubscriptionRequestParameters, SubscriptionResponse, SuccessResponse}
 import play.api.Logging
 import play.api.http.Status.CONFLICT
-import play.api.libs.json.JsSuccess
+import play.api.libs.json.{JsSuccess, Json}
 import uk.gov.hmrc.http.HttpReads.is2xx
 import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse, UpstreamErrorResponse}
 
 import java.time.LocalDateTime
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
+import uk.gov.hmrc.http.{HttpReads, HttpResponse}
+
 class SubscriptionConnector @Inject() (val userAnswersConnectors: UserAnswersConnectors, val config: FrontendAppConfig, val http: HttpClient)
     extends Logging {
   val subscriptionUrl = s"${config.pillar2BaseUrl}/report-pillar2-top-up-taxes/subscription/create-subscription"
 
-  def crateSubscription(subscriptionParameter: SubscriptionRequestParameters)(implicit
-    hc:                                        HeaderCarrier,
-    ec:                                        ExecutionContext
-  ): Future[Option[SubscriptionResponse]] =
-    http
-      .POST[SubscriptionRequestParameters, HttpResponse](subscriptionUrl, subscriptionParameter)
-      .map { response =>
-        logger.info(s"Response received with status ${response.status} and body: ${response.body}")
+  implicit val lenientHttpReads: HttpReads[HttpResponse] = new HttpReads[HttpResponse] {
+    override def read(method: String, url: String, response: HttpResponse): HttpResponse = response
+  }
 
+  def crateSubscription(
+    subscriptionParameter: SubscriptionRequestParameters
+  )(implicit hc:           HeaderCarrier, ec: ExecutionContext): Future[Option[SubscriptionResponse]] =
+    http
+      .POST[SubscriptionRequestParameters, HttpResponse](subscriptionUrl, subscriptionParameter)(
+        wts = Json.writes[SubscriptionRequestParameters],
+        rds = lenientHttpReads,
+        hc = hc,
+        ec = ec
+      )
+      .map { response =>
         response.status match {
           case status if is2xx(status) =>
             response.json.validate[SuccessResponse] match {
               case JsSuccess(successResponse, _) =>
-                logger.info("Successfully deserialized response")
                 Some(successResponse.success)
               case _ =>
                 logger.error("Failed to deserialize success response")
                 None
             }
+          case CONFLICT =>
+            logger.error(s"Conflict detected with body: ${response.body}")
+            Some(SubscriptionResponse("", "", LocalDateTime.now(), Some(ErrorResponse("Conflict detected during subscription"))))
           case _ =>
-            logger.warn(s"Subscription call failed with status ${response.status}")
+            logger.warn(s"Unhandled status received: ${response.status}")
             None
         }
       }
-      .recover {
-
-        case UpstreamErrorResponse(message, CONFLICT, _, _) =>
-          logger.error(s"Conflict detected with body: $message")
-          Some(SubscriptionResponse("", "", LocalDateTime.now(), Some(ErrorResponse("Conflict detected during subscription"))))
-
-        case e: Exception =>
-          logger.error(s"Exception occurred during subscription creation: ${e.getMessage}", e)
-          None
+      .recover { case e: Exception =>
+        logger.error(s"Exception occurred during subscription creation: ${e.getMessage}", e)
+        None
       }
 
 }
