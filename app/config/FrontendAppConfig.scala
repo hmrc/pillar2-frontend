73830bfbee6e95b92164efbe456d1e25127c805d
aashik-varahan <151053998+aashik-varahan@users.noreply.github.com>
2024-01-31 14:46:29 +0000

    Service banner routes unauthenticated users to start page

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -42,6 +42,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
   val loginUrl:         String = configuration.get[String]("urls.login")
   val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
   val signOutUrl:       String = configuration.get[String]("urls.signOut")
+  val startPageUrl:     String = configuration.get[String]("urls.startPage")
 
   val enrolmentKey:            String = configuration.get[String](s"keys.enrolmentKey.pillar2")
   lazy val pillar2FrontendUrl: String = configuration.get[String]("urls.pillar2-frontend")
