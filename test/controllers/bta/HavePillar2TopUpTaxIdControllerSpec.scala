7da6275699874bd093e4badd2c9c5848cad9ed69
Richy Jassal <20478717+jassalrichy@users.noreply.github.com>
2024-02-06 15:41:15 +0000

    [PIL-637] - link to eacd home page from pillar2-id question

--- test/controllers/bta/HavePillar2TopUpTaxIdControllerSpec.scala
+++ test/controllers/bta/HavePillar2TopUpTaxIdControllerSpec.scala
@@ -110,7 +110,7 @@ class HavePillar2TopUpTaxIdControllerontrollerSpec extends SpecBase {
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual controllers.routes.UnderConstructionController.onPageLoad.url
+        redirectLocation(result).value mustEqual appConfig.eacdHomePageUrl
       }
     }
 
