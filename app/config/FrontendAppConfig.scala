11e15b89826fd5bf6ed4068bf36e770ce55f0c5f
aashik-varahan <151053998+aashik-varahan@users.noreply.github.com>
2024-02-12 12:09:32 +0000

    Resolving format errors

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -108,7 +108,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
   val eacdHomePageUrl:              String  = configuration.get[String]("urls.eacdHomePage")
   val howToRegisterPlr2GuidanceUrl: String  = configuration.get[String]("urls.howToRegisterPlr2Guidance")
 
-  val rfmAccessEnabled:    Boolean = configuration.get[Boolean]("features.rfmAccessEnabled")
+  val rfmAccessEnabled: Boolean = configuration.get[Boolean]("features.rfmAccessEnabled")
 
   def allowlistEnabled:    Boolean     = configuration.getOptional[Boolean]("filters.allowlist.enabled").getOrElse(false)
   lazy val allowListedIps: Seq[String] = configuration.get[Seq[String]]("filters.allowlist.ips")
