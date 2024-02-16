8d5299710ff34f910b2f9af9529a5727e6061996
aashik-varahan <151053998+aashik-varahan@users.noreply.github.com>
2024-02-16 10:54:34 +0000

    Added UpeStartPageController and View class

---
+++ app/controllers/rfm/UpeStartPageRegistrationController.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.rfm
+
+import config.FrontendAppConfig
+import controllers.actions.IdentifierAction
+import models.Mode
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.rfm.UpeStartPageRegistrationView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class UpeStartPageRegistrationController @Inject() (
+                                                  identify:                 IdentifierAction,
+                                                  val controllerComponents: MessagesControllerComponents,
+                                                  view:                     UpeStartPageRegistrationView
+                                                )(implicit ec:              ExecutionContext, appConfig: FrontendAppConfig)
+  extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = identify { implicit request =>
+    Ok(view(mode))
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = identify.async { implicit request =>
+    Future.successful(Redirect(controllers.rfm.routes.UpeNameRegistrationController.onPageLoad(mode)))
+  }
+}
