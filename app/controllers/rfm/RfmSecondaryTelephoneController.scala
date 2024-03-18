ce9121cd0f1683d623d875312d937381b97bf157
aashik-varahan <151053998+aashik-varahan@users.noreply.github.com>
2024-03-18 15:35:39 +0000

    Adding RFM Secondary Contact Details

---
+++ app/controllers/rfm/RfmSecondaryTelephoneController.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.rfm
+
+import config.FrontendAppConfig
+import connectors.UserAnswersConnectors
+import controllers.actions._
+import forms.SecondaryTelephoneFormProvider
+import models.Mode
+import pages.{subSecondaryCapturePhonePage, subSecondaryContactNamePage, subSecondaryPhonePreferencePage}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class RfmSecondaryTelephoneController @Inject()(
+  val userAnswersConnectors: UserAnswersConnectors,
+  identify:                  IdentifierAction,
+  getData:                   DataRetrievalAction,
+  requireData:               DataRequiredAction,
+  formProvider:              SecondaryTelephoneFormProvider,
+  val controllerComponents:  MessagesControllerComponents,
+  view:                      SecondaryTelephoneView
+)(implicit ec:               ExecutionContext, appConfig: FrontendAppConfig)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    (for {
+      contactName <- request.userAnswers.get(subSecondaryContactNamePage)
+      _           <- request.userAnswers.get(subSecondaryPhonePreferencePage)
+    } yield {
+      val form = formProvider(contactName)
+      val preparedForm = request.userAnswers.get(subSecondaryCapturePhonePage) match {
+        case Some(v) => form.fill(v)
+        case None    => form
+      }
+      Ok(view(preparedForm, mode, contactName))
+
+    })
+      .getOrElse(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
+
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    request.userAnswers
+      .get(subSecondaryContactNamePage)
+      .map { contactName =>
+        val form = formProvider(contactName)
+        form
+          .bindFromRequest()
+          .fold(
+            formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode, contactName))),
+            value =>
+              for {
+                updatedAnswers <- Future.fromTry(request.userAnswers.set(subSecondaryCapturePhonePage, value))
+                _              <- userAnswersConnectors.save(updatedAnswers.id, Json.toJson(updatedAnswers.data))
+              } yield Redirect(controllers.subscription.routes.CaptureSubscriptionAddressController.onPageLoad(mode))
+          )
+      }
+      .getOrElse(Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad())))
+  }
+
+}
---
+++ app/controllers/rfm/RfmSecondaryTelephonePreferenceController.scala
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.rfm
+
+import config.FrontendAppConfig
+import connectors.UserAnswersConnectors
+import controllers.actions._
+import forms.SecondaryTelephonePreferenceFormProvider
+import models.Mode
+import pages.{subSecondaryContactNamePage, subSecondaryEmailPage, subSecondaryPhonePreferencePage}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class RfmSecondaryTelephonePreferenceController @Inject()(
+  val userAnswersConnectors: UserAnswersConnectors,
+  identify:                  IdentifierAction,
+  getData:                   DataRetrievalAction,
+  requireData:               DataRequiredAction,
+  formProvider:              SecondaryTelephonePreferenceFormProvider,
+  val controllerComponents:  MessagesControllerComponents,
+  view:                      SecondaryTelephonePreferenceView
+)(implicit ec:               ExecutionContext, appConfig: FrontendAppConfig)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    (for {
+      _           <- request.userAnswers.get(subSecondaryEmailPage)
+      contactName <- request.userAnswers.get(subSecondaryContactNamePage)
+    } yield {
+      val form = formProvider(contactName)
+      val preparedForm = request.userAnswers.get(subSecondaryPhonePreferencePage) match {
+        case Some(v) => form.fill(v)
+        case None    => form
+      }
+      Ok(view(preparedForm, mode, contactName))
+
+    })
+      .getOrElse(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
+
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    request.userAnswers
+      .get(subSecondaryContactNamePage)
+      .map { contactName =>
+        val form = formProvider(contactName)
+        form
+          .bindFromRequest()
+          .fold(
+            formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode, contactName))),
+            value =>
+              value match {
+                case true =>
+                  for {
+                    updatedAnswers <- Future.fromTry(request.userAnswers.set(subSecondaryPhonePreferencePage, value))
+                    _              <- userAnswersConnectors.save(updatedAnswers.id, Json.toJson(updatedAnswers.data))
+                  } yield Redirect(controllers.subscription.routes.SecondaryTelephoneController.onPageLoad(mode))
+                case false =>
+                  for {
+                    updatedAnswers <- Future.fromTry(request.userAnswers.set(subSecondaryPhonePreferencePage, value))
+                    _              <- userAnswersConnectors.save(updatedAnswers.id, Json.toJson(updatedAnswers.data))
+                  } yield Redirect(controllers.subscription.routes.CaptureSubscriptionAddressController.onPageLoad(mode))
+              }
+          )
+      }
+      .getOrElse(Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad())))
+  }
+
+}
