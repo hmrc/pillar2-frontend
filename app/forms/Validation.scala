0ea6c8d831c483a3acdb978a209093738a52590e
aashik-varahan <151053998+aashik-varahan@users.noreply.github.com>
2024-06-20 15:23:03 +0100

    Modified Telephone Regex pattern for Repayments

--- app/forms/Validation.scala
+++ app/forms/Validation.scala
@@ -19,9 +19,10 @@ package forms
 object Validation {
   final val EMAIL_REGEX =
     """^(?!\.)("([^"\r\\]|\\["\r\\])*"|([-a-zA-Z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"""
-  final val GROUPID_REGEX   = "^X[A-Z]PLR[0-9]{10}$"
-  final val TELEPHONE_REGEX = "^[0-9 +()]{0,25}$"
-  final val BIC_SWIFT_REGEX = "^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?$"
-  final val IBAN_REGEX      = "^[A-Z]{2}[0-9]{2}[0-9A-Z]{10,30}$"
-  final val MONETARY_REGEX  = """^-?(\d*(\.\d{1,2})?)$"""
+  final val GROUPID_REGEX              = "^X[A-Z]PLR[0-9]{10}$"
+  final val TELEPHONE_REGEX            = "^[0-9 +()]{0,25}$"
+  final val REPAYMENTS_TELEPHONE_REGEX = "^[0-9 +()]{0,50}$"
+  final val BIC_SWIFT_REGEX            = "^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?$"
+  final val IBAN_REGEX                 = "^[A-Z]{2}[0-9]{2}[0-9A-Z]{10,30}$"
+  final val MONETARY_REGEX             = """^-?(\d*(\.\d{1,2})?)$"""
 }
