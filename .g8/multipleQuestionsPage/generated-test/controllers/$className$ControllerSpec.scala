c2ffed9878577a96b9bde7dae78191b46909f532
alikouhestani <alikouhestani1@gmail.com>
2024-06-25 09:01:18 +0100

    multipleQuestions initial

--- .g8/multipleQuestionsPage/generated-test/controllers/$className$ControllerSpec.scala
+++ .g8/multipleQuestionsPage/generated-test/controllers/$className$ControllerSpec.scala
@@ -6,6 +6,11 @@ import models.{NormalMode, $className$, UserAnswers}
 import pages.$className$Page
 import play.api.libs.json.Json
 import play.api.mvc.Call
+import scala.concurrent.Future
+import play.api.inject.bind
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchersSugar.eqTo
+import org.mockito.Mockito.{verify, when}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import views.html.$className$View
@@ -16,18 +21,7 @@ class $className$ControllerSpec extends SpecBase {
 
   val formProvider = new $className$FormProvider()
 
-  def controller(): $className$Controller =
-    new $className$Controller(
-      mockUserAnswersConnectors,
-      preAuthenticatedActionBuilders,
-      preDataRetrievalActionImpl,
-      preDataRequiredActionImpl,
-      formProvider,
-      stubMessagesControllerComponents(),
-      view$className$
-    )
-
-  val userAnswers = UserAnswers(
+  private val validAnswer = UserAnswers(
     userAnswersId,
     Json.obj(
       $className$Page.toString -> Json.obj(
@@ -57,7 +51,7 @@ class $className$ControllerSpec extends SpecBase {
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(validAnswer)).build()
 
       running(application) {
         val request = FakeRequest(GET, routes.$className$Controller.onPageLoad(NormalMode).url)
@@ -71,9 +65,6 @@ class $className$ControllerSpec extends SpecBase {
           view(formProvider().fill($className$("value 1", "value 2")), NormalMode)(request, appConfig(application), messages(application)).toString
       }
     }
-
-
-
     "must return a Bad Request and errors when invalid data is submitted" in {
 
       val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
@@ -91,6 +82,26 @@ class $className$ControllerSpec extends SpecBase {
 
         status(result) mustEqual BAD_REQUEST
         contentAsString(result) mustEqual view(boundForm, NormalMode)(request, appConfig(application), messages(application)).toString
+      }
+    }
+
+    "redirect to under construction page in case of a valid submission and save the relevant data" in {
+      val expectedUserAnswers = emptyUserAnswers.setOrException($className$Page, validAnswer)
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[UserAnswersConnectors].toInstance(mockUserAnswersConnectors)
+          )
+          .build()
+      running(application) {
+        when(mockUserAnswersConnectors.save(any(), any())(any())).thenReturn(Future(Json.toJson(Json.obj())))
+        val request =
+          FakeRequest(POST, routes.$className$Controller.onSubmit(NormalMode).url)
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.UnderConstructionController.onPageLoad.url
+        verify(mockUserAnswersConnectors).save(eqTo(expectedUserAnswers.id), eqTo(expectedUserAnswers.data))(any())
       }
     }
 
