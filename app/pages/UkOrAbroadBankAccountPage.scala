43592aca2d8815781cfa662488c41f92a6268b93
Javed Iqbal <javed.iqbal1@digital.hmrc.gov.uk>
2024-06-28 11:38:48 +0100

    PIL-148 cleaning other bankacount related detail if different bank account is selected

--- app/pages/UkOrAbroadBankAccountPage.scala
+++ app/pages/UkOrAbroadBankAccountPage.scala
@@ -16,12 +16,45 @@
 
 package pages
 
-import models.UkOrAbroadBankAccount
+import models.{UkOrAbroadBankAccount, UserAnswers}
 import play.api.libs.json.JsPath
-
+import scala.util.Try
 case object UkOrAbroadBankAccountPage extends QuestionPage[UkOrAbroadBankAccount] {
 
   override def path: JsPath = JsPath \ toString
 
   override def toString: String = "ukOrAbroadBankAccount"
+
+  override def cleanup(value: Option[UkOrAbroadBankAccount], userAnswers: UserAnswers): Try[UserAnswers] =
+    if (value.contains(UkOrAbroadBankAccount.ForeignBankAccount)) {
+      userAnswers
+        .remove(BankAccountDetailsPage)
+        .flatMap(
+          _.remove(RepaymentsContactNamePage).flatMap(
+            _.remove(RepaymentsContactEmailPage).flatMap(
+              _.remove(UpeContactEmailPage).flatMap(
+                _.remove(RepaymentsContactByTelephonePage).flatMap(
+                  _.remove(RepaymentsTelephoneDetailsPage)
+                )
+              )
+            )
+          )
+        )
+    } else if (value.contains(UkOrAbroadBankAccount.UkBankAccount)) {
+      userAnswers
+        .remove(NonUKBankPage)
+        .flatMap(
+          _.remove(RepaymentsContactNamePage).flatMap(
+            _.remove(RepaymentsContactEmailPage).flatMap(
+              _.remove(UpeContactEmailPage).flatMap(
+                _.remove(RepaymentsContactByTelephonePage).flatMap(
+                  _.remove(RepaymentsTelephoneDetailsPage)
+                )
+              )
+            )
+          )
+        )
+    } else {
+      super.cleanup(value, userAnswers)
+    }
 }
