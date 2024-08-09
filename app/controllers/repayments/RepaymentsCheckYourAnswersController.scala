ae5224c501a0ef22035b6e4fcb12f4835083c8e0
Richy Jassal <20478717+jassalrichy@users.noreply.github.com>
2024-08-09 15:18:19 +0100

    [PIL-1070] - Spinning wheel waiting for Repayment submission response

--- app/controllers/repayments/RepaymentsCheckYourAnswersController.scala
+++ app/controllers/repayments/RepaymentsCheckYourAnswersController.scala
@@ -17,9 +17,9 @@
 package controllers.repayments
 
 import cats.data.OptionT
-import cats.implicits.catsSyntaxApplicativeError
 import config.FrontendAppConfig
 import controllers.actions._
+import models.repayments.RepaymentsStatus.{IncompleteDataError, SuccessfullyCompleted, UnexpectedResponseError}
 import models.{UnexpectedResponse, UserAnswers}
 import pages._
 import play.api.Logging
@@ -34,7 +34,6 @@ import views.html.repayments.RepaymentsCheckYourAnswersView
 
 import javax.inject.{Inject, Named}
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
 
 class RepaymentsCheckYourAnswersController @Inject() (
   override val messagesApi:               MessagesApi,
@@ -52,42 +51,39 @@ class RepaymentsCheckYourAnswersController @Inject() (
     with Logging {
 
   def onPageLoad(): Action[AnyContent] =
-    (featureAction.repaymentsAccessAction andThen identify andThen getSessionData andThen requireSessionData) { implicit request =>
+    (featureAction.repaymentsAccessAction andThen identify andThen getSessionData andThen requireSessionData).async { implicit request =>
       implicit val userAnswers: UserAnswers = request.userAnswers
       userAnswers.get(RepaymentCompletionStatus) match {
-        case Some(true) => Redirect(controllers.repayments.routes.RepaymentErrorReturnController.onPageLoad())
-        case _          => Ok(view(listRefund(), listBankAccountDetails(), contactDetailsList()))
+        case Some(true) => Future.successful(Redirect(controllers.repayments.routes.RepaymentErrorReturnController.onPageLoad()))
+        case _ =>
+          for {
+            updatedAnswers <- Future.fromTry(userAnswers.remove(RepaymentsStatusPage))
+            _              <- sessionRepository.set(updatedAnswers)
+          } yield Ok(view(listRefund(), listBankAccountDetails(), contactDetailsList()))
       }
     }
 
   def onSubmit(): Action[AnyContent] =
-    (featureAction.repaymentsAccessAction andThen identify andThen getSessionData andThen requireSessionData).async { implicit request =>
-      (for {
-        repaymentData        <- OptionT.fromOption[Future](repaymentService.getRepaymentData(request.userAnswers))
-        _                    <- OptionT.liftF(repaymentService.sendRepaymentDetails(repaymentData))
-        updatedAnswers       <- OptionT.liftF(Future.fromTry(request.userAnswers.set(RepaymentCompletionStatus, true)))
-        clearedRepaymentData <- OptionT.liftF(Future.fromTry(clearRepaymentDetails(updatedAnswers)))
-        _                    <- OptionT.liftF(sessionRepository.set(clearedRepaymentData))
-      } yield Redirect(controllers.repayments.routes.RepaymentConfirmationController.onPageLoad()))
-        .recover { case UnexpectedResponse =>
-          Redirect(controllers.repayments.routes.RepaymentErrorController.onPageLoadRepaymentSubmissionFailed)
+    (featureAction.repaymentsAccessAction andThen identify andThen getSessionData andThen requireSessionData) { implicit request =>
+      val repaymentsStatus = (for {
+        repaymentData <- OptionT.fromOption[Future](repaymentService.getRepaymentData(request.userAnswers))
+        _             <- OptionT.liftF(repaymentService.sendRepaymentDetails(repaymentData))
+      } yield SuccessfullyCompleted).value
+        .flatMap {
+          case Some(result) => Future.successful(result)
+          case _            => Future.successful(IncompleteDataError)
         }
-        .getOrElse(Redirect(controllers.repayments.routes.RepaymentsIncompleteDataController.onPageLoad))
+        .recover {
+          case UnexpectedResponse => UnexpectedResponseError
+          case _: Exception => IncompleteDataError
+        }
+      for {
+        updatedStatus  <- repaymentsStatus
+        updatedAnswers <- Future.fromTry(request.userAnswers.set(RepaymentsStatusPage, updatedStatus))
+        _              <- sessionRepository.set(updatedAnswers)
+      } yield (): Unit
+      Redirect(controllers.repayments.routes.RepaymentsWaitingRoomController.onPageLoad())
     }
-
-  private def clearRepaymentDetails(userAnswers: UserAnswers): Try[UserAnswers] =
-    for {
-      updatedUserAnswers1  <- userAnswers.remove(RepaymentAccountNameConfirmationPage)
-      updatedUserAnswers2  <- updatedUserAnswers1.remove(RepaymentsContactByTelephonePage)
-      updatedUserAnswers3  <- updatedUserAnswers2.remove(RepaymentsContactEmailPage)
-      updatedUserAnswers4  <- updatedUserAnswers3.remove(RepaymentsContactNamePage)
-      updatedUserAnswers5  <- updatedUserAnswers4.remove(RepaymentsRefundAmountPage)
-      updatedUserAnswers6  <- updatedUserAnswers5.remove(RepaymentsTelephoneDetailsPage)
-      updatedUserAnswers7  <- updatedUserAnswers6.remove(UkOrAbroadBankAccountPage)
-      updatedUserAnswers8  <- updatedUserAnswers7.remove(ReasonForRequestingRefundPage)
-      updatedUserAnswers9  <- updatedUserAnswers8.remove(NonUKBankPage)
-      updatedUserAnswers10 <- updatedUserAnswers9.remove(BankAccountDetailsPage)
-    } yield updatedUserAnswers10
 
   private def contactDetailsList()(implicit messages: Messages, userAnswers: UserAnswers) =
     SummaryListViewModel(
---
+++ app/controllers/repayments/RepaymentsWaitingRoomController.scala
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.repayments
+
+import config.FrontendAppConfig
+import controllers.actions.{IdentifierAction, SessionDataRequiredAction, SessionDataRetrievalAction}
+import models.UserAnswers
+import models.repayments.RepaymentsStatus._
+import pages.{BankAccountDetailsPage, NonUKBankPage, ReasonForRequestingRefundPage, RepaymentAccountNameConfirmationPage, RepaymentCompletionStatus, RepaymentsContactByTelephonePage, RepaymentsContactEmailPage, RepaymentsContactNamePage, RepaymentsRefundAmountPage, RepaymentsStatusPage, RepaymentsTelephoneDetailsPage, UkOrAbroadBankAccountPage}
+import play.api.Logging
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.repayments.RepaymentsWaitingRoomView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
+
+class RepaymentsWaitingRoomController @Inject()(
+  identify:                 IdentifierAction,
+  getData:                  SessionDataRetrievalAction,
+  requireData:              SessionDataRequiredAction,
+  sessionRepository:        SessionRepository,
+  val controllerComponents: MessagesControllerComponents,
+  view:                     RepaymentsWaitingRoomView
+)(implicit ec:                            ExecutionContext, appConfig:       FrontendAppConfig)
+    extends FrontendBaseController
+    with I18nSupport
+    with Logging {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    request.userAnswers
+      .get(RepaymentsStatusPage) match {
+      case Some(SuccessfullyCompleted)    =>
+        for {
+          updatedAnswers       <- Future.fromTry(request.userAnswers.set(RepaymentCompletionStatus, true))
+          clearedRepaymentData <- Future.fromTry(clearRepaymentDetails(updatedAnswers))
+          _                    <- sessionRepository.set(clearedRepaymentData)
+        } yield Redirect(controllers.repayments.routes.RepaymentConfirmationController.onPageLoad())
+      case Some(UnexpectedResponseError)  =>
+        Future.successful(Redirect(controllers.repayments.routes.RepaymentErrorController.onPageLoadRepaymentSubmissionFailed))
+      case Some(IncompleteDataError)      => Future.successful(Redirect(controllers.repayments.routes.RepaymentsIncompleteDataController.onPageLoad))
+      case s                              => Future.successful(Ok(view(s)))
+    }
+
+  }
+
+  private def clearRepaymentDetails(userAnswers: UserAnswers): Try[UserAnswers] =
+    for {
+      updatedUserAnswers1  <- userAnswers.remove(RepaymentAccountNameConfirmationPage)
+      updatedUserAnswers2  <- updatedUserAnswers1.remove(RepaymentsContactByTelephonePage)
+      updatedUserAnswers3  <- updatedUserAnswers2.remove(RepaymentsContactEmailPage)
+      updatedUserAnswers4  <- updatedUserAnswers3.remove(RepaymentsContactNamePage)
+      updatedUserAnswers5  <- updatedUserAnswers4.remove(RepaymentsRefundAmountPage)
+      updatedUserAnswers6  <- updatedUserAnswers5.remove(RepaymentsTelephoneDetailsPage)
+      updatedUserAnswers7  <- updatedUserAnswers6.remove(UkOrAbroadBankAccountPage)
+      updatedUserAnswers8  <- updatedUserAnswers7.remove(ReasonForRequestingRefundPage)
+      updatedUserAnswers9  <- updatedUserAnswers8.remove(NonUKBankPage)
+      updatedUserAnswers10 <- updatedUserAnswers9.remove(BankAccountDetailsPage)
+    } yield updatedUserAnswers10
+
+}
