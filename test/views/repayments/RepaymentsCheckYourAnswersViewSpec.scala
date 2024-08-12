f582f6098dae4afce94d8314e881b25b42cd7fed
javed.iqbal1 <131012491+javed-iqbal1@users.noreply.github.com>
2024-08-12 16:01:32 +0100

    helloooo
    Signed-off-by: javed.iqbal1 <131012491+javed-iqbal1@users.noreply.github.com>

--- test/views/repayments/RepaymentsCheckYourAnswersViewSpec.scala
+++ test/views/repayments/RepaymentsCheckYourAnswersViewSpec.scala
@@ -87,6 +87,7 @@ class RepaymentsCheckYourAnswersViewSpec extends ViewSpecBase {
       view.getElementsByClass("govuk-summary-list__key").get(3).text  must include("Name of the bank")
 
 
+
       view.getElementsByClass("govuk-summary-list__key").get(4).text  must include("Name on account")
 
 
