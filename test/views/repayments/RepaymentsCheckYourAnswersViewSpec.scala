2ec4c20c290188dc8ca1a4e5c3d5a2d58f4c3293
javed.iqbal1 <131012491+javed-iqbal1@users.noreply.github.com>
2024-08-12 15:32:32 +0100

    dsdfs..................................................................last

--- test/views/repayments/RepaymentsCheckYourAnswersViewSpec.scala
+++ test/views/repayments/RepaymentsCheckYourAnswersViewSpec.scala
@@ -86,6 +86,10 @@ class RepaymentsCheckYourAnswersViewSpec extends ViewSpecBase {
       view.getElementsByClass("govuk-summary-list__key").get(2).text  must include("What type of account will the refund be sent to?")
       view.getElementsByClass("govuk-summary-list__key").get(3).text  must include("Name of the bank")
       view.getElementsByClass("govuk-summary-list__key").get(4).text  must include("Name on account")
+
+
+
+
       view.getElementsByClass("govuk-summary-list__key").get(5).text  must include("BIC or SWIFT code")
       view.getElementsByClass("govuk-summary-list__key").get(6).text  must include("IBAN")
 
