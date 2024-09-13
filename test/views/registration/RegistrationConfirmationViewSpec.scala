9595f2ca1b3d774c21ff138b7b96d74187a77cab
MarjanMubarok <46748482+MarjanMubarok@users.noreply.github.com>
2024-09-13 14:21:24 +0100

    PIL-1022 - Add missing unit test

---
+++ test/views/registration/RegistrationConfirmationViewSpec.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.registration
+
+import base.ViewSpecBase
+import models.MneOrDomestic
+import models.MneOrDomestic.Uk
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import views.html.RegistrationConfirmationView
+
+class RegistrationConfirmationViewSpec extends ViewSpecBase {
+  val testPillar2ID   = "PLR2ID123"
+  val testCompanyName = "TestCompany"
+  val testDate        = "13 September 2024"
+  val testTimeStamp   = "11:00am (GMT)"
+  val testMneOrDomestic: MneOrDomestic = Uk
+
+  val page: RegistrationConfirmationView = inject[RegistrationConfirmationView]
+  val view: Document =
+    Jsoup.parse(page(testPillar2ID, testCompanyName, testDate, testTimeStamp, testMneOrDomestic)(request, appConfig, messages).toString())
+
+  "Registration Confirmation View" should {
+    "have a title" in {
+      view.getElementsByTag("title").text must include("Registration complete - Report Pillar 2 top-up taxes - GOV.UK")
+    }
+
+    "have a panel" in {
+      view.getElementsByClass("govuk-panel__title").text must include("Registration complete")
+      view.getElementsByClass("govuk-panel__body").text  must include("Group’s Pillar 2 top-up taxes ID PLR2ID123")
+    }
+
+    "have a heading" in {
+      view.getElementsByTag("h2").first.text must include(s"Registration date: ")
+    }
+
+    "have the correct paragraphs" in {
+      view.getElementsByClass("govuk-body").get(0).text must include(
+        "TestCompany has successfully registered to report for " +
+          "Domestic Top-up Tax and Multinational Top-up Tax, " +
+          "on 13 September 2024 at 11:00am (GMT)."
+      )
+
+      view.getElementsByClass("govuk-body").get(1).text must include(
+        "You will be able to find your Pillar 2 top-up taxes ID and " +
+          "registration date on your account homepage. Keep these details safe."
+      )
+
+      view.getElementsByClass("govuk-body").get(2).text must include(
+        "You can now report and manage your Pillar 2 top-up taxes."
+      )
+    }
+
+    "have a bullet list with download and print links" in {
+      val bulletItems = view.getElementsByClass("govuk-list--bullet").select("li")
+
+      bulletItems.get(0).text must include("Download as PDF")
+      bulletItems.get(1).text must include("Print this page")
+    }
+
+    "have warning text" in {
+      view.getElementsByClass("govuk-warning-text__text").text must include(
+        "You will not be emailed a confirmation of this registration."
+      )
+    }
+
+  }
+}
