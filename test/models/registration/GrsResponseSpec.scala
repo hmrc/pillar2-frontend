d8243100c091cab3aeb63071a98ff03bceaea80e
JamesMMiller <7511223+JamesMMiller@users.noreply.github.com>
2024-10-18 09:13:40 +0100

    Add optional field test

--- test/models/registration/GrsResponseSpec.scala
+++ test/models/registration/GrsResponseSpec.scala
@@ -235,6 +235,48 @@ class GrsResponseSpec extends AnyFreeSpec with Matchers {
         // Ensure unexpected field is ignored
         (json \ "unexpectedField").as[String] mustBe "Some value"
       }
+
+      "when JSON contains incorporatedEntityRegistrationData without optional fields" in {
+        val json = Json.parse(
+          """
+          {
+            "incorporatedEntityRegistrationData": {
+              "companyProfile": {
+                "companyName": "Test Company",
+                "companyNumber": "12345678",
+                "unsanitisedCHROAddress": {
+                  "address_line_1": "123 Test Street",
+                  "country": "United Kingdom",
+                  "postal_code": "TE1 1ST"
+                }
+              },
+              "ctutr": "1234567890",
+              "identifiersMatch": true,
+              "registration": {
+                "registrationStatus": "REGISTERED",
+                "registeredBusinessPartnerId": "X1234567"
+              }
+            }
+          }
+          """
+        )
+
+        val result = json.as[GrsResponse]
+        result.incorporatedEntityRegistrationData must not be None
+        result.partnershipEntityRegistrationData mustBe None
+
+        result.incorporatedEntityRegistrationData.foreach { data =>
+          data.companyProfile.companyName mustBe "Test Company"
+          data.companyProfile.companyNumber mustBe "12345678"
+          data.companyProfile.dateOfIncorporation mustBe None
+          data.companyProfile.unsanitisedCHROAddress.address_line_1 mustBe Some("123 Test Street")
+          data.ctutr mustBe "1234567890"
+          data.identifiersMatch mustBe true
+          data.businessVerification mustBe None
+          data.registration.registrationStatus mustBe RegistrationStatus.Registered
+          data.registration.registeredBusinessPartnerId mustBe Some("X1234567")
+        }
+      }
     }
   }
 }
