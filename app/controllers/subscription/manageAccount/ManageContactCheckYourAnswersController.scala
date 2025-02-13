105ee50e9cd47c55908bd669a1abe955c7421de8
olakunlekuye2 <100710237+olakunlekuye2@users.noreply.github.com>
2025-02-13 11:04:53 +0000

    Updated logic and tests

--- app/controllers/subscription/manageAccount/ManageContactCheckYourAnswersController.scala
+++ app/controllers/subscription/manageAccount/ManageContactCheckYourAnswersController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,13 +17,11 @@
 package controllers.subscription.manageAccount
 
 import cats.data.OptionT
-import cats.implicits.catsSyntaxApplicativeError
-import com.google.inject.Inject
 import config.FrontendAppConfig
 import controllers.actions.{IdentifierAction, SubscriptionDataRequiredAction, SubscriptionDataRetrievalAction}
-import controllers.routes
-import models.UnexpectedResponse
-import pages.AgentClientPillar2ReferencePage
+import models.subscription.ManageContactDetailsStatus
+import models.{InternalIssueError, UserAnswers}
+import pages.{AgentClientPillar2ReferencePage, ManageContactDetailsStatusPage}
 import play.api.Logging
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
@@ -35,8 +33,9 @@ import viewmodels.checkAnswers.manageAccount._
 import viewmodels.govuk.summarylist._
 import views.html.subscriptionview.manageAccount.ManageContactCheckYourAnswersView
 
-import javax.inject.Named
+import javax.inject.{Inject, Named}
 import scala.concurrent.{ExecutionContext, Future}
+
 class ManageContactCheckYourAnswersController @Inject() (
   @Named("EnrolmentIdentifier") identify: IdentifierAction,
   getData:                                SubscriptionDataRetrievalAction,
@@ -52,47 +51,75 @@ class ManageContactCheckYourAnswersController @Inject() (
     with I18nSupport
     with Logging {
 
-  def onPageLoad(): Action[AnyContent] =
-    (identify andThen getData andThen requireData) { implicit request =>
-      val primaryContactList = SummaryListViewModel(
-        rows = Seq(
-          ContactNameComplianceSummary.row(),
-          ContactEmailAddressSummary.row(),
-          ContactByTelephoneSummary.row(),
-          ContactCaptureTelephoneDetailsSummary.row()
-        ).flatten
-      ).withCssClass("govuk-!-margin-bottom-9")
+  def onPageLoad(): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    sessionRepository.get(request.userId).flatMap {
+      case None => Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
+      case Some(answers) =>
+        answers.get(ManageContactDetailsStatusPage) match {
+          case Some(ManageContactDetailsStatus.InProgress) =>
+            Future.successful(
+              Redirect(controllers.subscription.manageAccount.routes.ManageContactDetailsWaitingRoomController.onPageLoad)
+            )
+          case _ =>
+            val primaryContactList = SummaryListViewModel(
+              rows = Seq(
+                ContactNameComplianceSummary.row(),
+                ContactEmailAddressSummary.row(),
+                ContactByTelephoneSummary.row(),
+                ContactCaptureTelephoneDetailsSummary.row()
+              ).flatten
+            )
 
-      val secondaryContactList = SummaryListViewModel(
-        rows = Seq(
-          AddSecondaryContactSummary.row(),
-          SecondaryContactNameSummary.row(),
-          SecondaryContactEmailSummary.row(),
-          SecondaryTelephonePreferenceSummary.row(),
-          SecondaryTelephoneSummary.row()
-        ).flatten
-      ).withCssClass("govuk-!-margin-bottom-9")
+            val secondaryContactList = SummaryListViewModel(
+              rows = Seq(
+                AddSecondaryContactSummary.row(),
+                SecondaryContactNameSummary.row(),
+                SecondaryContactEmailSummary.row(),
+                SecondaryTelephonePreferenceSummary.row(),
+                SecondaryTelephoneSummary.row()
+              ).flatten
+            )
 
-      val address = SummaryListViewModel(
-        rows = Seq(ContactCorrespondenceAddressSummary.row(countryOptions)).flatten
-      ).withCssClass("govuk-!-margin-bottom-9")
+            val address = SummaryListViewModel(
+              rows = Seq(ContactCorrespondenceAddressSummary.row(countryOptions)).flatten
+            )
 
-      Ok(view(primaryContactList, secondaryContactList, address))
-    }
-
-  def onSubmit(): Action[AnyContent] =
-    (identify andThen getData andThen requireData).async { implicit request =>
-      (for {
-        userAnswers <- OptionT.liftF(sessionRepository.get(request.userId))
-        referenceNumber <- OptionT
-                             .fromOption[Future](userAnswers.flatMap(_.get(AgentClientPillar2ReferencePage)))
-                             .orElse(OptionT.fromOption[Future](referenceNumberService.get(None, enrolments = Some(request.enrolments))))
-        _ <- OptionT.liftF(subscriptionService.amendContactOrGroupDetails(request.userId, referenceNumber, request.subscriptionLocalData))
-      } yield Redirect(controllers.routes.DashboardController.onPageLoad))
-        .recover { case UnexpectedResponse =>
-          Redirect(routes.ViewAmendSubscriptionFailedController.onPageLoad)
+            Future.successful(Ok(view(primaryContactList, secondaryContactList, address)))
         }
-        .getOrElse(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+    }
+  }
+
+  def onSubmit(): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    logger.info(s"[ManageContactCheckYourAnswers] Submission started for user ${request.userId}")
+    val result = for {
+      userAnswers <- OptionT.liftF(sessionRepository.get(request.userId))
+      referenceNumber <- OptionT
+                           .fromOption[Future](userAnswers.flatMap(_.get(AgentClientPillar2ReferencePage)))
+                           .orElse(OptionT.fromOption[Future](referenceNumberService.get(None, enrolments = Some(request.enrolments))))
+      _ <- OptionT.liftF(subscriptionService.amendContactOrGroupDetails(request.userId, referenceNumber, request.subscriptionLocalData))
+      updatedAnswers = userAnswers match {
+                         case Some(answers) => answers.setOrException(ManageContactDetailsStatusPage, ManageContactDetailsStatus.InProgress)
+                         case None =>
+                           UserAnswers(request.userId).setOrException(ManageContactDetailsStatusPage, ManageContactDetailsStatus.InProgress)
+                       }
+      _ <- OptionT.liftF(sessionRepository.set(updatedAnswers))
+    } yield {
+      logger.info(s"[ManageContactCheckYourAnswers] Redirecting to waiting room for ${request.userId}")
+      Redirect(controllers.subscription.manageAccount.routes.ManageContactDetailsWaitingRoomController.onPageLoad)
     }
 
+    result.value
+      .recover {
+        case InternalIssueError =>
+          logger.error(s"[ManageContactCheckYourAnswers] Submission failed for ${request.userId} due to InternalIssueError")
+          Some(Redirect(controllers.routes.ViewAmendSubscriptionFailedController.onPageLoad))
+        case e: Exception =>
+          logger.error(s"[ManageContactCheckYourAnswers] Submission failed for ${request.userId}: ${e.getMessage}")
+          Some(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
+      }
+      .map(_.getOrElse {
+        logger.error(s"[ManageContactCheckYourAnswers] Submission failed for ${request.userId}")
+        Redirect(controllers.routes.JourneyRecoveryController.onPageLoad())
+      })
+  }
 }
--- app/controllers/subscription/manageAccount/ManageContactDetailsWaitingRoomController.scala
+++ app/controllers/subscription/manageAccount/ManageContactDetailsWaitingRoomController.scala
@@ -1,3 +1,76 @@
-package controllers.subscription.manageAccount class ManageContactDetailsWaitingRoomController {
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package controllers.subscription.manageAccount
+
+import config.FrontendAppConfig
+import controllers.actions.{IdentifierAction, SubscriptionDataRetrievalAction}
+import controllers.routes
+import models.subscription.ManageContactDetailsStatus
+import pages.ManageContactDetailsStatusPage
+import play.api.Logging
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.subscriptionview.manageAccount.ManageContactDetailsWaitingRoomView
+
+import javax.inject.{Inject, Named}
+import scala.concurrent.{ExecutionContext, Future}
+
+class ManageContactDetailsWaitingRoomController @Inject() (
+  @Named("EnrolmentIdentifier") identify: IdentifierAction,
+  getData:                                SubscriptionDataRetrievalAction,
+  val controllerComponents:               MessagesControllerComponents,
+  view:                                   ManageContactDetailsWaitingRoomView,
+  sessionRepository:                      SessionRepository
+)(implicit ec:                            ExecutionContext, appConfig: FrontendAppConfig)
+    extends FrontendBaseController
+    with I18nSupport
+    with Logging {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen getData).async { implicit request =>
+    logger.info(s"[ManageContactDetailsWaitingRoom] Loading waiting room for user ${request.userId}")
+
+    sessionRepository
+      .get(request.userId)
+      .flatMap { refreshedAnswers =>
+        val status = refreshedAnswers.flatMap(_.get(ManageContactDetailsStatusPage))
+        logger.info(s"[ManageContactDetailsWaitingRoom] Current status for ${request.userId}: $status")
+
+        status match {
+          case Some(ManageContactDetailsStatus.SuccessfullyCompleted) =>
+            logger.info(s"[ManageContactDetailsWaitingRoom] SuccessfullyCompleted detected for ${request.userId}, redirecting to dashboard")
+            Future.successful(Redirect(controllers.routes.DashboardController.onPageLoad))
+
+          case Some(ManageContactDetailsStatus.InProgress) =>
+            logger.info(s"[ManageContactDetailsWaitingRoom] InProgress status for ${request.userId}, re-rendering spinner")
+            sessionRepository.set(
+              refreshedAnswers.get.setOrException(ManageContactDetailsStatusPage, ManageContactDetailsStatus.SuccessfullyCompleted)
+            )
+            Future.successful(Ok(view(Some(ManageContactDetailsStatus.InProgress))))
+
+          case _ =>
+            logger.warn(s"[ManageContactDetailsWaitingRoom] Missing or unexpected status for ${request.userId}, redirecting to error page")
+            Future.successful(Redirect(routes.ViewAmendSubscriptionFailedController.onPageLoad))
+        }
+      }
+      .recover { case ex =>
+        logger.error(s"[ManageContactDetailsWaitingRoom] Error while loading waiting room for ${request.userId}: ${ex.getMessage}")
+        Redirect(routes.ViewAmendSubscriptionFailedController.onPageLoad)
+      }
+  }
 }
