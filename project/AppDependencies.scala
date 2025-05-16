8c160f86d9995bcabe58114f082264d4a2d28b7b
pankaj2453 <12529426+pankaj2453@users.noreply.github.com>
2025-05-16 12:40:02 +0100

    PIL-2005: update play-frontend version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   val bootstrapVersion = "9.11.0"
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % "12.0.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % "12.1.0",
     "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                    % mongoVersion,
