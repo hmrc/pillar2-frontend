c45c3cb3d0c9716fd17f1a4ac310a2151b30790d
MoDweik <180322631+MoDweik@users.noreply.github.com>
2025-05-23 21:47:28 +0100

    lint

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -99,7 +99,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
   val opsBaseUrl:             String  = servicesConfig.baseUrl("ops")
   val opsStartUrl:            String  = configuration.get[String]("microservice.services.ops.startUrl")
   val enablePayByBankAccount: Boolean = configuration.get[Boolean]("features.enablePayByBankAccount")
-  val pillar2ResearchUrl: String = configuration.get[String]("urls.pillar2Research")
+  val pillar2ResearchUrl:     String  = configuration.get[String]("urls.pillar2Research")
 
   def transactionHistoryEndDate: LocalDate = {
     val date = configuration.get[String]("features.transactionHistoryEndDate")
