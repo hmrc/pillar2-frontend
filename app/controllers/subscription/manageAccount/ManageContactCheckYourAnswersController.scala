f0b09e8743d034965d75aaeb5d35475816bd4a53
JamesMMiller <7511223+JamesMMiller@users.noreply.github.com>
2025-06-05 13:53:28 +0100

    Update ManageContactCheckYourAnswersController onSubmit

--- app/controllers/subscription/manageAccount/ManageContactCheckYourAnswersController.scala
+++ app/controllers/subscription/manageAccount/ManageContactCheckYourAnswersController.scala
@@ -19,7 +19,7 @@ package controllers.subscription.manageAccount
 import cats.data.OptionT
 import config.FrontendAppConfig
 import controllers.actions.{IdentifierAction, SubscriptionDataRequiredAction, SubscriptionDataRetrievalAction}
-import models.subscription.ManageContactDetailsStatus
+import models.subscription.{ManageContactDetailsStatus, SubscriptionLocalData}
 import models.{InternalIssueError, UnexpectedResponse, UserAnswers}
 import pages.{AgentClientPillar2ReferencePage, ManageContactDetailsStatusPage}
 import play.api.Logging
@@ -27,6 +27,8 @@ import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import services.{ReferenceNumberService, SubscriptionService}
+import uk.gov.hmrc.auth.core.Enrolment
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import utils.countryOptions.CountryOptions
 import viewmodels.checkAnswers.manageAccount._
@@ -91,38 +93,52 @@ class ManageContactCheckYourAnswersController @Inject() (
 
   def onSubmit(): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
     logger.info(s"[ManageContactCheckYourAnswers] Submission started for user ${request.userId}")
-    val result = for {
-      userAnswers <- OptionT.liftF(sessionRepository.get(request.userId))
-      referenceNumber <- OptionT
-                           .fromOption[Future](userAnswers.flatMap(_.get(AgentClientPillar2ReferencePage)))
-                           .orElse(OptionT.fromOption[Future](referenceNumberService.get(None, enrolments = Some(request.enrolments))))
-      _ <- OptionT.liftF(subscriptionService.amendContactOrGroupDetails(request.userId, referenceNumber, request.subscriptionLocalData))
+
+    for {
+      userAnswers <- sessionRepository.get(request.userId)
       updatedAnswers = userAnswers match {
                          case Some(answers) => answers.setOrException(ManageContactDetailsStatusPage, ManageContactDetailsStatus.InProgress)
                          case None =>
                            UserAnswers(request.userId).setOrException(ManageContactDetailsStatusPage, ManageContactDetailsStatus.InProgress)
                        }
-      _ <- OptionT.liftF(sessionRepository.set(updatedAnswers))
+      _ <- sessionRepository.set(updatedAnswers)
     } yield {
-      logger.info(s"[ManageContactCheckYourAnswers] Redirecting to waiting room for ${request.userId}")
+      updateSubscriptionInBackground(request.userId, request.subscriptionLocalData, request.enrolments)
       Redirect(controllers.subscription.manageAccount.routes.ManageContactDetailsWaitingRoomController.onPageLoad)
     }
+  }
+
+  private def updateSubscriptionInBackground(userId: String, subscriptionData: SubscriptionLocalData, enrolments: Set[Enrolment])(implicit
+    hc:                                              HeaderCarrier,
+    ec:                                              ExecutionContext
+  ): Future[Unit] = {
+    val result = for {
+      userAnswers <- OptionT.liftF(sessionRepository.get(userId))
+      referenceNumber <- OptionT
+                           .fromOption[Future](userAnswers.flatMap(_.get(AgentClientPillar2ReferencePage)))
+                           .orElse(OptionT.fromOption[Future](referenceNumberService.get(None, enrolments = Some(enrolments))))
+      _ <- OptionT.liftF(subscriptionService.amendContactOrGroupDetails(userId, referenceNumber, subscriptionData))
+      updatedAnswers = userAnswers match {
+                         case Some(answers) =>
+                           answers.setOrException(ManageContactDetailsStatusPage, ManageContactDetailsStatus.SuccessfullyCompleted)
+                         case None =>
+                           UserAnswers(userId).setOrException(ManageContactDetailsStatusPage, ManageContactDetailsStatus.SuccessfullyCompleted)
+                       }
+      _ <- OptionT.liftF(sessionRepository.set(updatedAnswers))
+    } yield ()
 
     result.value
       .recover {
         case InternalIssueError =>
-          logger.error(s"[ManageContactCheckYourAnswers] Submission failed for ${request.userId} due to InternalIssueError")
-          Some(Redirect(controllers.routes.ViewAmendSubscriptionFailedController.onPageLoad))
+          logger.error(s"[ManageContactCheckYourAnswers] Subscription update failed for $userId due to InternalIssueError")
+          None
         case UnexpectedResponse =>
-          logger.error(s"[ManageContactCheckYourAnswers] Submission failed for ${request.userId} due to UnexpectedResponse")
-          Some(Redirect(controllers.routes.ViewAmendSubscriptionFailedController.onPageLoad))
+          logger.error(s"[ManageContactCheckYourAnswers] Subscription update failed for $userId due to UnexpectedResponse")
+          None
         case e: Exception =>
-          logger.error(s"[ManageContactCheckYourAnswers] Submission failed for ${request.userId}: ${e.getMessage}")
-          Some(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
+          logger.error(s"[ManageContactCheckYourAnswers] Subscription update failed for $userId: ${e.getMessage}")
+          None
       }
-      .map(_.getOrElse {
-        logger.error(s"[ManageContactCheckYourAnswers] Submission failed for ${request.userId}")
-        Redirect(controllers.routes.JourneyRecoveryController.onPageLoad())
-      })
+      .map(_ => ())
   }
 }
