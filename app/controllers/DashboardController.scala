325668d560e478ee752ead3d4af2d34e97ab38eb
olakunlekuye2 <100710237+olakunlekuye2@users.noreply.github.com>
2025-06-20 11:03:42 +0100

    updated logging

--- app/controllers/DashboardController.scala
+++ app/controllers/DashboardController.scala
@@ -72,7 +72,7 @@ class DashboardController @Inject() (
                          .readAndCacheSubscription(ReadSubscriptionRequestParameters(request.userId, referenceNumber))
                          .recover {
                            case InternalIssueError =>
-                             logger.info(s"DashboardController - subscription is in progress for PLR reference: $referenceNumber")
+                             logger.error(s"DashboardController - subscription is in progress for PLR reference: $referenceNumber")
                              throw new RuntimeException("REGISTRATION_IN_PROGRESS:" + referenceNumber)
                            case other => throw other
                          }
