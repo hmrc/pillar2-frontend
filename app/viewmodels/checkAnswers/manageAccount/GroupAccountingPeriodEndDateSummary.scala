29aea4c7a204c4aac0ed979f0a52ce09b9e324a6
Kevin Coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2025-07-17 14:54:09 +0100

    PIL-2153: Update the content for the manage group details (group's View) (#479)
    Update the content for the group details section
    Update the content for the Group details entity Location 
    Update the content for the Group details AP
    
    See scenarios here:
    https://jira.tools.tax.service.gov.uk/browse/PIL-2153

--- app/viewmodels/checkAnswers/manageAccount/GroupAccountingPeriodEndDateSummary.scala
+++ app/viewmodels/checkAnswers/manageAccount/GroupAccountingPeriodEndDateSummary.scala
@@ -33,14 +33,7 @@ object GroupAccountingPeriodEndDateSummary {
       val startDate = HtmlFormat.escape(dateHelper.formatDateGDS(answer.endDate))
       SummaryListRowViewModel(
         key = "groupAccountingEndDatePeriod.checkYourAnswersLabel",
-        value = ValueViewModel(HtmlContent(startDate)),
-        actions = Seq(
-          ActionItemViewModel(
-            "site.change",
-            controllers.subscription.manageAccount.routes.GroupAccountingPeriodController.onPageLoad.url
-          )
-            .withVisuallyHiddenText(messages("groupAccountingPeriod.change.hidden"))
-        )
+        value = ValueViewModel(HtmlContent(startDate))
       )
 
     }
