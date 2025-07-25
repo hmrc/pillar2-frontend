ca317196534958222e45d1c83d4b6950790270d8
Nikolas <gianick@users.noreply.github.com>
2025-07-25 19:33:33 +0100

    Fixed multiple H1s - views/rfm/RfmAddSecondaryContactViewSpec

--- test/views/rfm/RfmAddSecondaryContactViewSpec.scala
+++ test/views/rfm/RfmAddSecondaryContactViewSpec.scala
@@ -43,22 +43,24 @@ class RfmAddSecondaryContactViewSpec extends ViewSpecBase {
 
     "have a unique H1 heading" in {
       val h1Elements: Elements = view.getElementsByTag("h1")
-      // FIXME: this page has 2 H1 headings!!!
       h1Elements.size() mustBe 1
       h1Elements.text() mustBe pageTitle
     }
 
     "have two description paragraphs" in {
-      view.getElementsByClass("govuk-body").get(0).text must equal(
+      val paragraphs: Elements = view.getElementsByClass("govuk-body")
+
+      paragraphs.get(0).text must equal(
         "We use the secondary contact if we do not get a response from the primary contact. We encourage you to provide a secondary contact, if possible."
       )
-      view.getElementsByClass("govuk-body").get(1).text must equal(
+
+      paragraphs.get(1).text must equal(
         "This can be a team mailbox or another contact who is able to deal with enquiries about the group’s management of Pillar 2 Top-up Taxes."
       )
     }
 
-    "have a legend heading" in {
-      view.getElementsByClass("govuk-fieldset__heading").text must equal("Do you have a second contact?")
+    "have an H2 heading" in {
+      view.getElementsByTag("h2").text must include("Do you have a second contact?")
     }
 
     "have a button" in {
