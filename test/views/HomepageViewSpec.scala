cf84832fb7edee34faaa31465c4786f67fa25c03
olakunlekuye2 <100710237+olakunlekuye2@users.noreply.github.com>
2025-07-28 11:47:16 +0100

    Fix responsive card layout and remove redundant agentHasClientSetup parameter

--- test/views/HomepageViewSpec.scala
+++ test/views/HomepageViewSpec.scala
@@ -135,14 +135,14 @@ class HomepageViewSpec extends ViewSpecBase {
         )
       val returnsCard = organisationViewWithDueScenario.getElementsByClass("card-half-width").first()
 
-      returnsCard.getElementsByTag("h2").text() must include("Returns")
+      returnsCard.getElementsByTag("h2").text() mustBe "Returns"
 
       val cardHeader = returnsCard.getElementsByClass("card-label").first()
       val statusTag  = cardHeader.getElementsByClass("govuk-tag")
       statusTag.size() mustBe 0
 
       val links = returnsCard.getElementsByTag("a")
-      links.get(0).attr("href") must include("due-and-overdue-returns")
+      links.get(0).attr("href") mustBe controllers.dueandoverduereturns.routes.DueAndOverdueReturnsController.onPageLoad.url
       links.get(0).text() mustBe "View all due and overdue returns"
       links.get(1).text() mustBe "View submission history"
     }
@@ -165,7 +165,7 @@ class HomepageViewSpec extends ViewSpecBase {
       statusTag.first().attr("title") mustBe "Overdue returns"
 
       val links = returnsCard.getElementsByTag("a")
-      links.get(0).attr("href") must include("due-and-overdue-returns")
+      links.get(0).attr("href") mustBe controllers.dueandoverduereturns.routes.DueAndOverdueReturnsController.onPageLoad.url
       links.get(0).text() mustBe "View all due and overdue returns"
       links.get(1).text() mustBe "View submission history"
     }
