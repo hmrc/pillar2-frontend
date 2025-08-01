b628f935db02c33a1db2bb0c071a653b76df4a59
olakunlekuye2 <100710237+olakunlekuye2@users.noreply.github.com>
2025-08-01 17:49:31 +0100

    PIL-2173 Updated missing test case

--- test/views/subscriptionview/GroupAccountingPeriodViewSpec.scala
+++ test/views/subscriptionview/GroupAccountingPeriodViewSpec.scala
@@ -19,9 +19,12 @@ package views.subscriptionview
 import base.ViewSpecBase
 import forms.GroupAccountingPeriodFormProvider
 import models.NormalMode
+import models.subscription.AccountingPeriod
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import views.html.subscriptionview.GroupAccountingPeriodView
+
+import java.time.LocalDate
 
 class GroupAccountingPeriodViewSpec extends ViewSpecBase {
 
@@ -34,7 +37,7 @@ class GroupAccountingPeriodViewSpec extends ViewSpecBase {
   "GroupAccountingPeriodView" should {
 
     "have a title" in {
-      view.getElementsByTag("title").text must include("When did the group’s first accounting period start and end after 31 December 2023?")
+      view.getElementsByTag("title").text must include("When did the group\u2019s first accounting period start and end after 31 December 2023?")
     }
 
     "have a caption" in {
@@ -42,7 +45,7 @@ class GroupAccountingPeriodViewSpec extends ViewSpecBase {
     }
 
     "have a heading" in {
-      view.getElementsByTag("h1").get(0).text mustBe "When did the group’s first accounting period start and end after 31 December 2023?"
+      view.getElementsByTag("h1").get(0).text mustBe "When did the group\u2019s first accounting period start and end after 31 December 2023?"
     }
 
     "have the following paragraph content" in {
@@ -72,6 +75,19 @@ class GroupAccountingPeriodViewSpec extends ViewSpecBase {
 
     "have a button" in {
       view.getElementsByClass("govuk-button").text mustBe "Save and continue"
+    }
+
+    "show pre-populated values when form is filled with existing data" in {
+      val accountingPeriod = AccountingPeriod(LocalDate.of(2024, 1, 15), LocalDate.of(2025, 1, 15))
+      val filledForm       = formProvider().fill(accountingPeriod)
+      val viewWithData     = Jsoup.parse(page(filledForm, NormalMode)(request, appConfig, messages).toString())
+
+      viewWithData.getElementById("startDate.day").attr("value") mustBe "15"
+      viewWithData.getElementById("startDate.month").attr("value") mustBe "1"
+      viewWithData.getElementById("startDate.year").attr("value") mustBe "2024"
+      viewWithData.getElementById("endDate.day").attr("value") mustBe "15"
+      viewWithData.getElementById("endDate.month").attr("value") mustBe "1"
+      viewWithData.getElementById("endDate.year").attr("value") mustBe "2025"
     }
   }
 }
