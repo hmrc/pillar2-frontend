90213f51d72dc722257b2bf3e0b6461270b9e386
Nikolas <gianick@users.noreply.github.com>
2025-08-12 15:10:11 +0100

    Fixed BTNBeforeStartViewSpec

--- test/views/btn/BTNBeforeStartViewSpec.scala
+++ test/views/btn/BTNBeforeStartViewSpec.scala
@@ -23,7 +23,6 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
-
 import views.html.btn.BTNBeforeStartView
 
 class BTNBeforeStartViewSpec extends ViewSpecBase {
