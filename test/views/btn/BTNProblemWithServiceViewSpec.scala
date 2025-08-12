5c2896b114ef79c02d10443de6fd75f7a345745c
Nikolas <gianick@users.noreply.github.com>
2025-08-12 16:29:27 +0100

    Fixed BTNProblemWithServiceViewSpec

--- test/views/btn/BTNProblemWithServiceViewSpec.scala
+++ test/views/btn/BTNProblemWithServiceViewSpec.scala
@@ -18,7 +18,7 @@ package views.btn
 
 import base.ViewSpecBase
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
+import org.jsoup.nodes.{Document, Element}
 import org.jsoup.select.Elements
 import views.html.btn.BTNProblemWithServiceView
 
@@ -29,26 +29,27 @@ class BTNProblemWithServiceViewSpec extends ViewSpecBase {
   lazy val pageTitle: String                    = "Sorry, there is a problem with the service"
 
   "BTNProblemWithServiceView" should {
-    // FIXME: "have the correct page title" in {
     "have a title" in {
       view.title() mustBe s"$pageTitle - Report Pillar 2 Top-up Taxes - GOV.UK"
     }
 
-    // FIXME: we are using `select` here
-    "display the error heading" in {
+    "have a unique H1 heading" in {
       val h1Elements: Elements = view.getElementsByTag("h1")
       h1Elements.size() mustBe 1
       h1Elements.text() mustBe pageTitle
     }
 
     "display a try again later message" in {
-      view.select("p.govuk-body").text() must include("You can try again later when the service is available.")
+      view.getElementsByClass("govuk-body").first().text() mustBe "You can try again later when the service is available."
     }
 
     "have a return to home page link with the correct URL" in {
-      val link = view.select("a[href*='pillar2-top-up-tax-home']").first()
-      link.text() must include("Return to your account homepage to submit a Below-Threshold Notification again")
+      val link: Element = view.getElementsByClass("govuk-link").get(2)
+
+      link.text() mustBe "Return to your account homepage to submit a Below-Threshold Notification again"
       link.attr("href") mustBe controllers.routes.DashboardController.onPageLoad.url
+      link.attr("target") mustBe "_self"
+      link.attr("rel") mustNot be("noopener noreferrer")
     }
 
   }
