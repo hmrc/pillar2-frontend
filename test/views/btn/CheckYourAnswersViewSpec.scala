d0350627a1dfa7d50362168cf97daad4565de03e
Nikolas <gianick@users.noreply.github.com>
2025-08-15 12:05:55 +0100

    Fixed titles and matching H1s. Fixed failing tests.

--- test/views/btn/CheckYourAnswersViewSpec.scala
+++ test/views/btn/CheckYourAnswersViewSpec.scala
@@ -38,7 +38,7 @@ class CheckYourAnswersViewSpec extends ViewSpecBase {
   lazy val dateTimeFormatter: DateTimeFormatter    = DateTimeFormatter.ofPattern("d MMMM yyyy")
   lazy val accountingPeriod:  AccountingPeriod     = AccountingPeriod(startDate, endDate)
   lazy val page:              CheckYourAnswersView = inject[CheckYourAnswersView]
-  lazy val pageTitle:         String               = "Check your answers to submit your Below-Threshold Notification "
+  lazy val pageTitle:         String               = "Check your answers to submit your Below-Threshold Notification"
   lazy val validBTNCyaUa: UserAnswers = UserAnswers("id")
     .setOrException(SubAccountingPeriodPage, accountingPeriod)
     .setOrException(EntitiesInsideOutsideUKPage, true)
