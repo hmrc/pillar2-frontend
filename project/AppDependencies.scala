569d1d5c0dc216a138fc2140b0210753dc85d3b4
Nikolas <gianick@users.noreply.github.com>
2025-08-22 15:16:11 +0100

    NOJIRA buildsbt readme improvements (#515)
    - Updated-improved `README.md` file
    - Restructured and improved readability of `build.sbt`
    - Added a `checkCodeCoverage` alias to `build.sbt`
    - Restructured `Dependencies.scala` file
    - Fixed `scalafmt` warning `Default dialect is deprecated; use explicit:
    [Scala211,scala212,Scala212Source3,scala213,Scala213Source3,Sbt0137,Sbt1,scala3]`
    - Removed a duplicate `routes` entry
    - Fixed a typo in `test/helpers/ViewInstances.scala`
    - Updated libraries to latest versions
    - Fixed `services.FopServiceSpec` after upgrading
    `org.apache.xmlgraphics` to `2.11`
    (https://pdfbox.apache.org/3.0/migration.html#use-loader-to-get-a-pdf-document).
    This issue has been identified and fixed on
    [pillar2-submission-frontend](https://github.com/hmrc/pillar2-submission-frontend/pull/69)
    
    ---------
    
    Co-authored-by: MoDweik <180322631+MoDweik@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,25 +1,27 @@
 import sbt.*
 
 object AppDependencies {
+  val bootstrapVersion = "9.19.0"
   val mongoVersion     = "2.7.0"
-  val bootstrapVersion = "9.18.0"
+
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % "12.7.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % "12.8.0",
     "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                    % mongoVersion,
     "org.typelevel"         %% "cats-core"                             % "2.13.0",
-    "org.apache.xmlgraphics" % "fop"                                   % "2.10",
-    "commons-io"             % "commons-io"                            % "2.18.0",
+    "org.apache.pdfbox"      % "pdfbox"                                % "3.0.5",
+    "org.apache.xmlgraphics" % "fop"                                   % "2.11",
+    "commons-io"             % "commons-io"                            % "2.20.0",
     "com.beachape"          %% "enumeratum-play-json"                  % "1.9.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "org.scalatestplus"    %% "mockito-4-11"             % "3.2.18.0",
-    "org.jsoup"             % "jsoup"                    % "1.18.3",
+    "org.jsoup"             % "jsoup"                    % "1.21.1",
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"   % bootstrapVersion,
-    "org.mockito"          %% "mockito-scala"            % "1.17.37",
+    "org.mockito"          %% "mockito-scala"            % "2.0.0",
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30"  % mongoVersion,
     "org.scalatestplus"    %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.danielasfregola"  %% "random-data-generator"    % "2.9",
