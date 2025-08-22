28f2d5874f3354e524af180d004a387d0bba8f60
Nikolas <gianick@users.noreply.github.com>
2025-08-22 12:55:09 +0100

    Revered back to original approach in versions of Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,44 +1,31 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion                      = "9.19.0"
-  val catsCoreVersion                       = "2.13.0"
-  val commonsIoVersion                      = "2.20.0"
-  val enumeratumPlayJsonVersion             = "1.9.0"
-  val fopVersion                            = "2.11"
-  val jsoupVersion                          = "1.21.1"
-  val mockitoVersion                        = "3.2.18.0"
-  val mockitoScalaVersion                   = "2.0.0"
-  val mongoVersion                          = "2.7.0"
-  val pdfBoxVersion                         = "3.0.5"
-  val playConditionalFormMappingPlayVersion = "3.3.0"
-  val playFrontendHmrcPlayVersion           = "12.8.0"
-  val randomDataGeneratorVersion            = "2.9"
-  val scalacheckGenRegexpVersion            = "1.1.0"
-  val scalatestplusScalacheckVersion        = "3.1.0.0-RC2"
+  val bootstrapVersion = "9.19.0"
+  val mongoVersion     = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % playFrontendHmrcPlayVersion,
-    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % playConditionalFormMappingPlayVersion,
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % "12.8.0",
+    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                    % mongoVersion,
-    "org.typelevel"         %% "cats-core"                             % catsCoreVersion,
-    "org.apache.pdfbox"      % "pdfbox"                                % pdfBoxVersion,
-    "org.apache.xmlgraphics" % "fop"                                   % fopVersion,
-    "commons-io"             % "commons-io"                            % commonsIoVersion,
-    "com.beachape"          %% "enumeratum-play-json"                  % enumeratumPlayJsonVersion
+    "org.typelevel"         %% "cats-core"                             % "2.13.0",
+    "org.apache.pdfbox"      % "pdfbox"                                % "3.0.5",
+    "org.apache.xmlgraphics" % "fop"                                   % "2.11",
+    "commons-io"             % "commons-io"                            % "2.20.0",
+    "com.beachape"          %% "enumeratum-play-json"                  % "1.9.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatestplus"    %% "mockito-4-11"             % mockitoVersion,
-    "org.jsoup"             % "jsoup"                    % jsoupVersion,
+    "org.scalatestplus"    %% "mockito-4-11"             % "3.2.18.0",
+    "org.jsoup"             % "jsoup"                    % "1.21.1",
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"   % bootstrapVersion,
-    "org.mockito"          %% "mockito-scala"            % mockitoScalaVersion,
+    "org.mockito"          %% "mockito-scala"            % "2.0.0",
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30"  % mongoVersion,
-    "org.scalatestplus"    %% "scalatestplus-scalacheck" % scalatestplusScalacheckVersion,
-    "com.danielasfregola"  %% "random-data-generator"    % randomDataGeneratorVersion,
-    "io.github.wolfendale" %% "scalacheck-gen-regexp"    % scalacheckGenRegexpVersion
+    "org.scalatestplus"    %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
+    "com.danielasfregola"  %% "random-data-generator"    % "2.9",
+    "io.github.wolfendale" %% "scalacheck-gen-regexp"    % "1.1.0"
   ).map(_ % Test)
 
   val it: Seq[ModuleID] = Seq(
