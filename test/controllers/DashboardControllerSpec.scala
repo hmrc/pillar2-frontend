079c8fc8845be137404745fda259f1ee1a7dffb3
joshua-lamptey <220714391+joshua-lamptey@users.noreply.github.com>
2025-08-26 15:25:37 +0100

    [PIL-2353] - lint

--- test/controllers/DashboardControllerSpec.scala
+++ test/controllers/DashboardControllerSpec.scala
@@ -19,7 +19,7 @@ package controllers
 import base.SpecBase
 import controllers.actions.TestAuthRetrievals.Ops
 import generators.ModelGenerators
-import models.{UserAnswers, Due, Received, Incomplete, Overdue}
+import models.{Due, Incomplete, Overdue, Received, UserAnswers}
 import models.obligationsandsubmissions.ObligationStatus
 import models.subscription._
 import org.mockito.ArgumentMatchers.any
