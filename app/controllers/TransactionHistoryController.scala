e14997e42bb54f66c14e1f8cf016f947da7885c2
olakunlekuye2 <100710237+olakunlekuye2@users.noreply.github.com>
2025-09-03 14:27:17 +0100

    linting

--- app/controllers/TransactionHistoryController.scala
+++ app/controllers/TransactionHistoryController.scala
@@ -32,8 +32,8 @@ import uk.gov.hmrc.govukfrontend.views.Aliases.Text
 import uk.gov.hmrc.govukfrontend.views.viewmodels.pagination.{Pagination, PaginationItem, PaginationLink}
 import uk.gov.hmrc.govukfrontend.views.viewmodels.table.{HeadCell, Table, TableRow}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.paymenthistory.{NoTransactionHistoryView, TransactionHistoryErrorView, TransactionHistoryView}
 import views.ViewUtils.formattedCurrency
+import views.html.paymenthistory.{NoTransactionHistoryView, TransactionHistoryErrorView, TransactionHistoryView}
 
 import java.time.format.DateTimeFormatter
 import javax.inject.{Inject, Named}
