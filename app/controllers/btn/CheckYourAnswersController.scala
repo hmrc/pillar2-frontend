180a31e7d0465e8de9b91ea54936bb24a28107e3
olakunlekuye2 <100710237+olakunlekuye2@users.noreply.github.com>
2025-10-07 12:01:56 +0100

    initial commit

--- app/controllers/btn/CheckYourAnswersController.scala
+++ app/controllers/btn/CheckYourAnswersController.scala
@@ -22,15 +22,17 @@ import controllers.actions._
 import models.MneOrDomestic.Uk
 import models.audit.ApiResponseData
 import models.btn.{BTNRequest, BTNStatus}
+import models.UserAnswers
 import models.obligationsandsubmissions.AccountingPeriodDetails
 import models.subscription.AccountingPeriod
 import pages._
 import play.api.Logging
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
 import repositories.SessionRepository
-import services.BTNService
+import services.{BTNService, ObligationsAndSubmissionsService}
 import services.audit.AuditService
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import viewmodels.checkAnswers._
 import viewmodels.govuk.summarylist._
@@ -48,6 +50,7 @@ class CheckYourAnswersController @Inject() (
   view:                                   CheckYourAnswersView,
   cannotReturnView:                       BTNCannotReturnView,
   btnService:                             BTNService,
+  obligationsAndSubmissionsService:       ObligationsAndSubmissionsService,
   val controllerComponents:               MessagesControllerComponents,
   auditService:                           AuditService,
   checkPhase2Screens:                     Phase2ScreensAction,
@@ -107,67 +110,104 @@ class CheckYourAnswersController @Inject() (
           accountingPeriodTo = subAccountingPeriod.endDate
         )
 
-        implicit val pillar2Id: String = request.subscriptionLocalData.plrReference
+        implicit val pillar2Id: String        = request.subscriptionLocalData.plrReference
+        implicit val hc:        HeaderCarrier = uk.gov.hmrc.play.http.HeaderCarrierConverter.fromRequestAndSession(request, request.session)
 
-        val setProcessingF: Future[Unit] = for {
-          updatedAnswers <- Future.fromTry(userAnswers.set(BTNStatus, BTNStatus.processing))
-          _              <- sessionRepository.set(updatedAnswers)
-        } yield ()
-
-        setProcessingF.foreach { _ =>
-          btnService
-            .submitBTN(btnPayload)
-            .flatMap { resp =>
-              sessionRepository.get(request.userId).flatMap {
-                case Some(latest) =>
-                  for {
-                    submittedAnswers <- Future.fromTry(latest.set(BTNStatus, BTNStatus.submitted))
-                    _                <- sessionRepository.set(submittedAnswers)
-                    _ <- auditService.auditBTN(
-                           pillarReference = pillar2Id,
-                           accountingPeriod = subAccountingPeriod.toString,
-                           entitiesInsideAndOutsideUK = userAnswers.get(EntitiesInsideOutsideUKPage).getOrElse(false),
-                           apiResponseData = ApiResponseData(
-                             statusCode = CREATED,
-                             processingDate = resp.processingDate.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
-                             errorCode = None,
-                             responseMessage = "Success"
-                           )
-                         )
-                  } yield ()
-                case None =>
-                  Future.successful(())
-              }
-            }
-            .recover { case err =>
-              sessionRepository.get(request.userId).flatMap {
-                case Some(latest) =>
-                  for {
-                    errorAnswers <- Future.fromTry(latest.set(BTNStatus, BTNStatus.error))
-                    _            <- sessionRepository.set(errorAnswers)
-                    _ <- auditService.auditBTN(
-                           pillarReference = pillar2Id,
-                           accountingPeriod = subAccountingPeriod.toString,
-                           entitiesInsideAndOutsideUK = userAnswers.get(EntitiesInsideOutsideUKPage).getOrElse(false),
-                           apiResponseData = ApiResponseData(
-                             statusCode = INTERNAL_SERVER_ERROR,
-                             processingDate = java.time.LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
-                             errorCode = Some("InternalIssueError"),
-                             responseMessage = err.getMessage
-                           )
-                         )
-                  } yield ()
-                case None =>
-                  Future.successful(())
-              }
-            }
+       
+        checkUnderEnquiry(subAccountingPeriod).flatMap { isUnderEnquiry =>
+          if (isUnderEnquiry) {
+            logger.info(
+              s"BTN submission warning - accounting period under enquiry for period ${subAccountingPeriod.startDate} to ${subAccountingPeriod.endDate}"
+            )
+            Future.successful(Redirect(routes.BTNUnderEnquiryWarningController.onPageLoad))
+          } else {
+            submitBTNRequest(userAnswers, subAccountingPeriod, btnPayload, pillar2Id, request.userId)
+          }
         }
-
-        Future.successful(Redirect(routes.BTNWaitingRoomController.onPageLoad))
       case None =>
         logger.error("user answers not found")
         Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
     }
+  }
+
+  private def checkUnderEnquiry(accountingPeriod: AccountingPeriod)(implicit hc: HeaderCarrier, pillar2Id: String): Future[Boolean] =
+    obligationsAndSubmissionsService
+      .handleData(pillar2Id, accountingPeriod.startDate, accountingPeriod.endDate)
+      .map { obligationsData =>
+        obligationsData.accountingPeriodDetails
+          .find(period =>
+            period.startDate == accountingPeriod.startDate &&
+              period.endDate == accountingPeriod.endDate
+          )
+          .exists(_.underEnquiry)
+      }
+      .recover { case ex =>
+        logger.warn(s"Failed to check underEnquiry status: ${ex.getMessage}")
+        false 
+      }
+
+  private def submitBTNRequest(
+    userAnswers:         UserAnswers,
+    subAccountingPeriod: AccountingPeriod,
+    btnPayload:          BTNRequest,
+    pillar2Id:           String,
+    userId:              String
+  )(implicit hc:         HeaderCarrier): Future[Result] = {
+    val setProcessingF: Future[Unit] = for {
+      updatedAnswers <- Future.fromTry(userAnswers.set(BTNStatus, BTNStatus.processing))
+      _              <- sessionRepository.set(updatedAnswers)
+    } yield ()
+
+    setProcessingF.foreach { _ =>
+      btnService
+        .submitBTN(btnPayload)(hc, pillar2Id)
+        .flatMap { resp =>
+          sessionRepository.get(userId).flatMap {
+            case Some(latest) =>
+              for {
+                submittedAnswers <- Future.fromTry(latest.set(BTNStatus, BTNStatus.submitted))
+                _                <- sessionRepository.set(submittedAnswers)
+                _ <- auditService.auditBTN(
+                       pillarReference = pillar2Id,
+                       accountingPeriod = subAccountingPeriod.toString,
+                       entitiesInsideAndOutsideUK = userAnswers.get(EntitiesInsideOutsideUKPage).getOrElse(false),
+                       apiResponseData = ApiResponseData(
+                         statusCode = CREATED,
+                         processingDate = resp.processingDate.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                         errorCode = None,
+                         responseMessage = "Success"
+                       )
+                     )
+              } yield ()
+            case None =>
+              Future.successful(())
+          }
+        }
+        .recover { case err =>
+          sessionRepository.get(userId).flatMap {
+            case Some(latest) =>
+              for {
+                errorAnswers <- Future.fromTry(latest.set(BTNStatus, BTNStatus.error))
+                _            <- sessionRepository.set(errorAnswers)
+                _ <- auditService.auditBTN(
+                       pillarReference = pillar2Id,
+                       accountingPeriod = subAccountingPeriod.toString,
+                       entitiesInsideAndOutsideUK = userAnswers.get(EntitiesInsideOutsideUKPage).getOrElse(false),
+                       apiResponseData = ApiResponseData(
+                         statusCode = INTERNAL_SERVER_ERROR,
+                         processingDate = java.time.LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                         errorCode = Some("InternalIssueError"),
+                         responseMessage = err.getMessage
+                       )
+                     )
+              } yield ()
+            case None =>
+              Future.successful(())
+          }
+        }
+    }
+
+    Future.successful(Redirect(routes.BTNWaitingRoomController.onPageLoad))
   }
 
   def cannotReturnKnockback: Action[AnyContent] = (identify andThen checkPhase2Screens) { implicit request =>
