1ec2781e6ad0de6d854813900a8b645c68d19bea
joshua-lamptey <220714391+joshua-lamptey@users.noreply.github.com>
2025-11-28 11:40:05 +0000

    [PIL-2555] - address comments

--- test/views/dueandoverduereturns/DueAndOverdueReturnsViewSpec.scala
+++ test/views/dueandoverduereturns/DueAndOverdueReturnsViewSpec.scala
@@ -252,6 +252,18 @@ class DueAndOverdueReturnsViewSpec extends ViewSpecBase with ObligationsAndSubmi
         secondTableStatusTags.get(1).text mustBe "Due"
         secondTableStatusTags.get(1).attr("class") mustBe "govuk-tag govuk-tag--blue"
       }
+
+      "show the submission history description" in {
+        val submissionHistoryParagraph: Element =
+          view.select("p.govuk-body").stream.filter(p => p.text.contains("submission and submission amendment")).findFirst().get()
+        submissionHistoryParagraph.text mustBe "Details of submission and submission amendment dates."
+      }
+
+      "show the correct submission history link" in {
+        val submissionHistoryLink: Element = view.getElementsByClass("govuk-link").get(3)
+        submissionHistoryLink.attr("href") mustBe controllers.submissionhistory.routes.SubmissionHistoryController.onPageLoad.url
+      }
+
     }
 
     "displaying agent-specific content" when {
@@ -286,12 +298,6 @@ class DueAndOverdueReturnsViewSpec extends ViewSpecBase with ObligationsAndSubmi
 
       "displaying submission history section" must {
         lazy val view: Document = Jsoup.parse(page(emptyResponse, fromDate, toDate, agentView = true)(request, appConfig, messages).toString())
-
-        "show the agent-specific submission history description" in {
-          val submissionHistoryParagraph: Element =
-            view.select("p.govuk-body").stream.filter(p => p.text.contains("submission and submission amendment")).findFirst().get()
-          submissionHistoryParagraph.text mustBe "Details of submission and submission amendment dates."
-        }
       }
     }
   }
