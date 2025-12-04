41c6ccc6c40c4998684c474c123d94b2a3d3549a
Dan Smith <75583950+dc-smith@users.noreply.github.com>
2025-12-04 10:28:16 +0000

    Reapply "PIL-2196 Introduce generic spinning wheel component (#598)" … (#607)
    …(#605)
    
    This reverts commit a448c99d6574cdb9b4c2f6d9d66c15f044fd8f73, which
    reverted d3eb1caa0658ebcb5bc3e703adb8ba7d3b4c5c10. So this PR is
    essentially a duplicate of #598.
    
    Probably easiest to merge this after
    https://github.com/hmrc/pillar2-frontend/pull/601, and handle any
    conflicts here.

--- app/controllers/HomepageController.scala
+++ app/controllers/HomepageController.scala
@@ -76,10 +76,9 @@ class HomepageController @Inject() (
         updatedAnswers2 <- OptionT.liftF(Future.fromTry(updatedAnswers1.remove(RepaymentsStatusPage)))
         updatedAnswers3 <- OptionT.liftF(Future.fromTry(updatedAnswers2.remove(RepaymentCompletionStatus)))
         updatedAnswers4 <- OptionT.liftF(Future.fromTry(updatedAnswers3.remove(RfmStatusPage)))
-        updatedAnswers5 <- OptionT.liftF(Future.fromTry(updatedAnswers4.remove(RepaymentsWaitingRoomVisited)))
-        updatedAnswers6 <- OptionT.liftF(Future.fromTry(updatedAnswers5.remove(ManageGroupDetailsStatusPage)))
-        updatedAnswers7 <- OptionT.liftF(Future.fromTry(updatedAnswers6.remove(ManageContactDetailsStatusPage)))
-        _               <- OptionT.liftF(sessionRepository.set(updatedAnswers7))
+        updatedAnswers5 <- OptionT.liftF(Future.fromTry(updatedAnswers4.remove(ManageGroupDetailsStatusPage)))
+        updatedAnswers6 <- OptionT.liftF(Future.fromTry(updatedAnswers5.remove(ManageContactDetailsStatusPage)))
+        _               <- OptionT.liftF(sessionRepository.set(updatedAnswers6))
         result          <-
           OptionT.liftF {
             subscriptionService
