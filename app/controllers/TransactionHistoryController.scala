732d31bc4f653d1241eb83377646e5b37ec547cc
JamesMMiller <7511223+JamesMMiller@users.noreply.github.com>
2026-01-14 14:53:05 +0000

    Update param names

--- app/controllers/TransactionHistoryController.scala
+++ app/controllers/TransactionHistoryController.scala
@@ -58,16 +58,16 @@ class TransactionHistoryController @Inject() (
     with I18nSupport
     with Logging {
 
-  private def retrieveTransactions(plrReference: String, dateFrom: LocalDate, dateTo: LocalDate)(using
+  private def retrieveTransactions(plrReference: String, fromDate: LocalDate, toDate: LocalDate)(using
     hc: HeaderCarrier
   ): Future[Seq[Transaction]] =
     if appConfig.useAccountActivityApi then
       accountActivityConnector
-        .retrieveAccountActivity(plrReference, dateFrom, dateTo)
+        .retrieveAccountActivity(plrReference, fromDate, toDate)
         .map(_.toTransactions)
     else
       financialDataConnector
-        .retrieveTransactionHistory(plrReference, dateFrom, dateTo)
+        .retrieveTransactionHistory(plrReference, fromDate, toDate)
         .map(_.financialHistory)
 
   def onPageLoadTransactionHistory(page: Option[Int]): Action[AnyContent] =
