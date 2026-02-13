c781b723bfd17356435f949e615d05df2f7b3412
JamesMMiller <7511223+JamesMMiller@users.noreply.github.com>
2026-02-13 13:26:12 +0000

    Update test

--- test/models/AccountActivityResponseSpec.scala
+++ test/models/AccountActivityResponseSpec.scala
@@ -797,7 +797,7 @@ class AccountActivityResponseSpec extends SpecBase {
         transactionDetails = Seq(
           AccountActivityTransaction(
             transactionType = TransactionType.Debit,
-            transactionDesc = "Pillar 2 UK Tax Return Pillar 2 DTT",
+            transactionDesc = "UKTR - DTT",
             startDate = Some(LocalDate.of(2025, 1, 1)),
             endDate = Some(LocalDate.of(2025, 12, 31)),
             accruedInterest = Some(BigDecimal(500)),
